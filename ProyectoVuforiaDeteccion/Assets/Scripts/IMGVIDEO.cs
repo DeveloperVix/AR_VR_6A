67e46c33acfd7eff77a07fb204914f1f24934ed3
JOSE CARLOS MARZANA <car_ma20@yahoo.com.mx>
2019-02-20 19:13:42 -0600

    Implementacion_Video
    en este ejercicio se coloco un video con realidad aumentada y que funcione en base a una imagen

---
+++ ProyectoVuforiaDeteccion/Assets/Scripts/IMGVIDEO.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class IMGVIDEO : MonoBehaviour
+{
+    DefaultTrackableEventHandler statusIMG;
+    public VideoPlayer mividio;
+    bool videoPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusIMG = GetComponent<DefaultTrackableEventHandler>();
+    }
+
+    private void Update()
+    {
+        if (statusIMG.isDetected &&!videoPlaying) { 
+            Debug.Log("INICIA VIDEO lel");
+            PlayButton();
+        }
+        else if (!statusIMG.isDetected) { 
+            Debug.Log("PAUSA VIDEO QUE SID");
+            PauseButton();
+        }
+    }
+
+    public void PlayButton()
+    {
+        mividio.Play();
+        videoPlaying = true;
+    }
+
+    public void PauseButton()
+    {
+        mividio.Pause();
+        videoPlaying = false;
+    }
+
+    // Update is called once per frame
+
+}

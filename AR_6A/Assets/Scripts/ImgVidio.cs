906b9d4b8ffbcbf4141b387950054cc72d81d43e
DeveloperVix <vicomisterio@hotmail.com>
2019-02-21 16:26:28 -0600

    Proyecto AR
    Image target y video

---
+++ AR_6A/Assets/Scripts/ImgVidio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class ImgVidio : MonoBehaviour, ITrackableEventHandler
+{
+
+    protected TrackableBehaviour mTrackableBehaviour;
+    public VideoPlayer miVidio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if(mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Reproduzco Vidio");
+            miVidio.Play();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            Debug.Log("Pauso Vidio");
+            miVidio.Pause();
+        }
+        else
+        {
+            miVidio.Stop();
+        }
+    }
+}

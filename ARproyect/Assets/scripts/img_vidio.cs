b3af7c395800bb0691d78025ef55d558a6f241e6
Miguel-SilvaxDD <42754121+Miguel-SilvaxDD@users.noreply.github.com>
2019-02-20 19:06:05 -0600

    Ejercicio_de_imageTarget
    New Features: Se agrego un image target, se detecta y reproduce un video que agregamios, yambien se agrego un boton de pausa y reproduccion

---
+++ ARproyect/Assets/scripts/img_vidio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class img_vidio : MonoBehaviour
+{
+    DefaultTrackableEventHandler statusImg;
+    public VideoPlayer miVidio;
+
+    bool videoPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusImg = GetComponent<DefaultTrackableEventHandler>();
+    }
+
+    void Update()
+    {
+        if(statusImg.isDetected && !videoPlaying)
+        {
+            Debug.Log("Inicia vidio v:");
+            Playbutton();
+        }
+        else if(!statusImg.isDetected)
+        {
+            Debug.Log("Pausa vidio :v");
+            Pausebutton();
+        }
+    }
+
+    public void Playbutton()
+    {
+        miVidio.Play();
+        videoPlaying = true;
+    }
+
+    public void Pausebutton()
+    {
+        miVidio.Pause();
+        videoPlaying = false;
+    }
+}

2228e0ce84b31893f492ab36ef8fa7ef626cd12e
Benjamin E <36773982+dracober@users.noreply.github.com>
2019-02-21 03:07:43 -0600

    Ejercicio_en_clase_Video_RA
    new features:
    se añadio otro target a la base de datos donde incluimos un video mediante componentes de unity y vuforia
    
    bugs:

---
+++ AR_Project/Assets/Scripts/ImgVideo.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using Vuforia;
+
+public class ImgVideo : MonoBehaviour
+{
+
+    DefaultTrackableEventHandler statusImg;
+    public VideoPlayer miVideo;
+
+    bool videoPlaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusImg = GetComponent<DefaultTrackableEventHandler>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+
+
+        if (statusImg.isDetected && !videoPlaying) {
+            Debug.Log("inicia video");
+            PlayButtom();
+        }
+        else if (!statusImg.isDetected) {
+            Debug.Log("pausa video");
+            PauseButtom();
+                }
+    }
+
+    public void PlayButtom()
+    {
+        miVideo.Play();
+        videoPlaying = true;
+    }
+
+    public void PauseButtom()
+    {
+        miVideo.Pause();
+        videoPlaying = false;
+    }
+
+}

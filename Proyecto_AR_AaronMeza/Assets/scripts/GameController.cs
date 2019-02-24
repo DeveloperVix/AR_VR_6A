a92317cb3d69c325b2ccba393fbc32d3d3ff14bf
mezaaaron <36971712+mezaaaron@users.noreply.github.com>
2019-02-23 20:13:06 -0600

    Tarea_Botones
    Botones de reproducir y pausa

---
+++ Proyecto_AR_AaronMeza/Assets/scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.Video;
+public class GameController : MonoBehaviour
+{
+
+
+    public GameObject img;
+    public VideoPlayer video;
+    DefaultTrackableEventHandler script;
+    public Button play;
+    public Button pause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = img.GetComponent<DefaultTrackableEventHandler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (script.isDetected)
+        {
+            play.GetComponent<Animator>().Play("play");
+            pause.GetComponent<Animator>().Play("pause");
+        }
+        else if (!script.isDetected)
+        {
+            play.GetComponent<Animator>().Play("playSalida");
+            pause.GetComponent<Animator>().Play("pauseSAlir");
+        }
+
+
+    }
+
+
+
+    public void playV()
+    {
+        video.Play();
+    }
+
+    public void stopV()
+    {
+        video.Pause();
+    }
+}

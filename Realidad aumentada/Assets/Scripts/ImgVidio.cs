afcc01e0c07f378dfe507e2ce29df0b344c26161
CORSMAN <37158231+CORSMAN@users.noreply.github.com>
2019-02-24 15:07:30 -0600

    Asunto: Tarea Imagetarjet. Descripcion: se añadio un target que al detectarlo reproduce un video, tambien se añadieron un par de botones para pausar y reproducir el video.     New features: botones, tarjet, video.

---
+++ Realidad aumentada/Assets/Scripts/ImgVidio.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;//libreria para modificar videoplayer
+using Vuforia;
+
+public class ImgVidio : MonoBehaviour
+{
+    public Animator anim;
+    public DefaultTrackableEventHandler statusImg;
+    public VideoPlayer miVidio;
+
+    bool videoPlaying = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //anim = GetComponent<Animation>();
+        //statusImg = GetComponent<DefaultTrackableEventHandler>();
+    }
+
+
+    private void Update()
+    {
+        if (statusImg.isDetected && !videoPlaying)
+        {
+            Debug.Log("inicia video");
+            Playbutton();
+        }
+        else if (!statusImg.isDetected)
+        {
+            Debug.Log("Pauso video");
+            PauseButton();
+            anim.SetBool("Transicion", false);
+        }
+    }
+
+    public void Playbutton()
+    {
+        miVidio.Play();
+        anim.SetBool("Transicion", true);
+        Debug.Log("Playaaaaaaa");
+        videoPlaying = true;
+    }
+
+    public void PauseButton()
+    {
+        miVidio.Pause();
+        
+        videoPlaying = false;
+    }
+}

864f389ccdc7317e538ddeb4f3fbcc322d6e4b4f
Eddbros <36971892+Eddbros@users.noreply.github.com>
2019-02-27 19:12:27 -0600

    Avance2_Puzzle
    Se implementó una pantalla de Loading
    
    Se agregó un script que maneja la pantalla de carga al seleccionar el botón de play. llamado GameControllerUI
    
    Se agregaron los cambios entre menús  con eventos de Click
    
    Cambios en el nombre de la escena principal del AR

---
+++ Proyecto_AR/Assets/Scripts/GameControllerUI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameControllerUI : MonoBehaviour
+{
+    public GameObject objLoading;
+
+    private static GameControllerUI instance;
+
+    public static GameControllerUI Instance { get { return instance; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.LogError("Hay mas de una instancia!");
+        }
+
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");
+
+        objLoading.SetActive(false);
+    }
+
+
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+
+        while (async != null)
+        {
+            yield return null;
+        }
+    }
+
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+    
+}

5d1d952cd0948d12ef4c2c9899548184b507d042
mezaaaron <36971712+mezaaaron@users.noreply.github.com>
2019-02-27 19:09:54 -0600

    AvanceDosPuzzle
    Se agrego la UI del menu principal, el gamecontroller y la pantalla de carga

--- Proyecto_AR_AaronMeza/Assets/scripts/GameController.cs
+++ Proyecto_AR_AaronMeza/Assets/scripts/GameController.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 using UnityEngine.Video;
+
+
 public class GameController : MonoBehaviour
 {
 
---
+++ Proyecto_AR_AaronMeza/Assets/scripts/GameControllerUI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+
+public class GameControllerUI : MonoBehaviour
+{
+
+    private static GameControllerUI instance;
+
+    public GameObject objLoanding;
+
+    public static GameControllerUI Instance { get { return instance; } }
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.Log("Hay más de un instancia!!");
+        }
+
+        objLoanding = GameObject.Find("Canvas/BackgroundLoading");
+        objLoanding.SetActive(false);
+    }
+
+
+
+    
+
+
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        objLoanding.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+    public void ex()
+    {
+        Application.Quit();
+    }
+
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+        while(async != null)
+        {
+            yield return null;
+        }
+    }
+
+    #endregion
+}

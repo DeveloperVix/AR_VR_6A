d4d0b6bb6207e450ecadf1304cd2ea59a78e5eb7
Ulikander <>
2019-02-27 22:18:29 -0600

    Update_PuzzleAR_Parte2
    El botón settings ahora muestra el menú y oculta el MainMenu.
    El botón Exit en Setttings cierra el menú y abre el principal.
    El botón Play carga una escena con una corrutina y la abre.

---
+++ MartinezSandoval_PracticaAR/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonPlay()
+    {
+        StartCoroutine(LoadScene("PuzzleGame"));
+    }
+
+    IEnumerator LoadScene(string scene)
+    {
+        AsyncOperation load = SceneManager.LoadSceneAsync(scene);
+        yield return load.isDone;
+        load.allowSceneActivation = true;
+    }
+}

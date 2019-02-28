a92d3003d8a8379e9a5c29afc260489a3c827fc0
Benny Santoyo Pérez <maggot15aseeck@gmail.com>
2019-02-27 19:09:32 -0600

    Avance 2 Puzzle
    -Se agrego codigo para el controlador de UI del MainMenu y de la escena del AR
    -Se implemento una pantalla de carga

---
+++ Practica AR/Assets/Scripts/GameControllerUI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControllerUI : MonoBehaviour
+{
+    private static GameControllerUI instance;
+    public static GameControllerUI Instance { get { return instance; } }//Acceder a todo lo que tiene este codigo
+    //instance es para checar si ya existe otra instancia de la misma clase
+
+    public GameObject objLoading;
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);//que no se destruya cuando cargue una nueva escena
+        }
+        else
+        {
+            Debug.LogError("Hay mas de una instancia >:v");
+        }
+
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");//ACCEDEMOS A UN HIJO
+        objLoading.SetActive(false);
+    }
+
+    //REGION ES PARA ACOMODAR EL CODIGO
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);//AsyncOperation es el proceso de pantalla de carga
+
+        while(async != null)
+        {   
+            yield return null;
+            //yield return new WaitForSeconds(5f);
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+    /*void Update()
+    {
+        
+    }*/
+}

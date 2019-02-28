4db56de16d0537daed9c9aeef3b045939adc7d31
Zack098 <37155500+Zack098@users.noreply.github.com>
2019-02-27 19:09:38 -0600

    AvanzePuzzle2
    New Features: Se agregaron las acciones de los botones de ajustes, juego y la pantalla de carga

---
+++ RA_2/Assets/Scripts/GameControllerUI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControllerUI : MonoBehaviour
+{
+    private static GameControllerUI instance;
+
+    public GameObject objLoading;
+
+    public static GameControllerUI Instance { get { return instance; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.LogError("Hay más de una instancia!");
+        }
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");
+        objLoading.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");
+        Debug.Log("Estoy Activo");
+    }
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));//cargar la escena de forma asincronica
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);////cargar la escena de forma asincronica
+
+        while (async != null)
+        {
+            yield return null;
+        }
+    }
+
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+}

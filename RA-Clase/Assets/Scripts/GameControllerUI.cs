d3d031028dd75f84b2e41ca0d2e76a267ffed7fa
Victor Gutierrez <36493042+vicgots@users.noreply.github.com>
2019-02-27 19:11:40 -0600

    Avance_de_puzzle_2
    -new features: Se ha añadido un gamecontroller que será el que lleve el control del juego al momento de cambiar de escenas, guardando un objeto que tendra un image que funcionara como una pantalla de carga.
    Se ha implementado las acciones correspondientes a los botones del main menu y el back del menu de settings

---
+++ RA-Clase/Assets/Scripts/GameControllerUI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControllerUI : MonoBehaviour
+{
+    private static GameControllerUI instance;
+
+    public static GameControllerUI Instance { get { return Instance; }  }
+
+    public GameObject objLoading;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        else
+        {
+            Debug.LogError("Hay mas de una isntancia!!");
+        }
+
+
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");
+        objLoading.SetActive(false);
+    }
+
+
+    private void OnEnable()
+    {
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");
+        
+        Debug.Log("Estoy activo");
+    }
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+
+        while(async != null)
+        {
+            yield return null;
+        }
+    }
+
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+}

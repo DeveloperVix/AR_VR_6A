ac320802504afb52ffff77751ecc3f90ca9b2838
vicsay1 <36805512+vicsay1@users.noreply.github.com>
2019-02-27 19:36:00 -0600

    Avance_2_puzzle
    se añadio funcionalidades de cambio de escena a los botones y tambien se implemento un gamecontroller.

---
+++ ProyectoRA_LopezRuiz/Assets/script/controller_ui.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controller_ui : MonoBehaviour
+{
+
+    private static controller_ui instace;
+    public static controller_ui Instace {get { return instace; } }
+
+    public GameObject objLoading;
+
+    void Start()
+    {
+        objLoading.SetActive(false);
+        if (instace == null)
+        {
+            instace = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.LogError("hay mas de una instancial!!");
+        }
+
+        objLoading = GameObject.Find("Canvas/back");
+        objLoading.SetActive(false);
+    }
+
+|   private void OnEnable()
+    {
+        objLoading = GameObject.Find("Canvas/back");
+    }
+
+    #region buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+        while (async != null)
+        {
+            yield return null;
+        }
+    }
+
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+}

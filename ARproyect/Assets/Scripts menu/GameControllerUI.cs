d075fba603435085f86239ff8124997e3e9dfe39
Miguel-SilvaxDD <42754121+Miguel-SilvaxDD@users.noreply.github.com>
2019-02-27 19:11:56 -0600

    Avance_de_Puzzle2
    New Features: Se agrego la pantalla de carga, el cambio de escenas y un gamecotroller

---
+++ ARproyect/Assets/Scripts menu/GameControllerUI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControllerUI : MonoBehaviour
+{
+
+    private static GameControllerUI instance;
+    public static GameControllerUI Instance { get { return instance; } }
+
+    public GameObject objLoading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.LogError("Hay mas de una instancia!!!!!!!!!!!!!!!!!!!!!!! xdxd");
+        }
+
+        objLoading = GameObject.Find("Canvas/BackGroundLoading");
+        objLoading.SetActive(false);
+    }
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+        while(async != null)
+        {
+            yield return null;
+        }
+    }
+
+    #endregion
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}

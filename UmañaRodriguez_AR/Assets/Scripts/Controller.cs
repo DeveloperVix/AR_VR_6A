0e09b7150c3481bb1119d81097eb8f00fc7610f5
Emilio Umaña Rodríguez <emilioumanarodriguez@outlook.com>
2019-02-27 19:09:31 -0600

    Avance_de_puzzle_2
    Se agregaron los funcionamientos de los botones de la interfaz de usuario.

---
+++ UmañaRodriguez_AR/Assets/Scripts/Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller: MonoBehaviour
+{
+    public GameObject objLoading;
+
+    private static Controller instance;
+
+    public static Controller Instance { get { return instance; } }
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.LogError("Hay mas de una instancia");
+        }
+
+
+        objLoading = GameObject.Find("Canvas/LoadingMenu");
+        objLoading.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        objLoading = GameObject.Find("Canvas/LoadingScene");
+        Debug.Log("Está Activo");
+    }
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+
+        while (async != null)
+        {
+            yield return null;
+        }
+    }
+
+
+    #endregion
+
+}

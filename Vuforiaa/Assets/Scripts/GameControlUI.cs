4efe491951d7075c0936a0371af34bffb188d205
NessCAlle <42752157+NessCAlle@users.noreply.github.com>
2019-02-27 19:09:24 -0600

    Segundo Avance de puzzle
    Se agrego Script para cambio de canvas y ecena y se agrego la imagen de loadingo para cambio de esena

---
+++ Vuforiaa/Assets/Scripts/GameControlUI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameControlUI : MonoBehaviour
+{
+    private static GameControlUI instance;
+    public static GameControlUI Instance { get { return instance; } }
+
+    public GameObject objLoading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance = null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.LogError("Hay mas de una instancia");
+        }
+
+        objLoading = GameObject.Find("Canvas/Loading");
+        objLoading.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        objLoading = GameObject.Find("Canvas/Loading");
+        Debug.Log("Estoy Activo");
+    }
+
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+
+        while(async != null)
+        {
+            yield return null;
+        }
+    }
+    #endregion
+}

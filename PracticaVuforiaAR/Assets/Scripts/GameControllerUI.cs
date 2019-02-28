92660be149f84fc7769713ef497742d4cc581292
BastianZavala <BastianZavala@users.noreply.github.com>
2019-02-27 19:10:25 -0600

    Avance2puzzles
    1-Se agrego una escena de loading...
    2-Se implemento un codigo de controladores de botones para los cambios de escena en la aplicacion.
    3-Los botones de settings, exit, back y play ya funcionan y mandan a la pantalla correspondiente

---
+++ PracticaVuforiaAR/Assets/Scripts/GameControllerUI.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameControllerUI : MonoBehaviour
+{
+    private static GameControllerUI instance;
+    public static GameControllerUI Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public GameObject objLoading;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.LogError("Hay mas de una instancia");
+        }
+
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");
+
+        objLoading.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        objLoading = GameObject.Find("Canvas/BackgrounLoading");
+        Debug.Log("Im Here Nigga");
+    }
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)                    //pediremos que escena quiere que cargue
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));                    //cambia de escena
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+
+        while (async != null)                                   //se indica la cantidad de tiempo de espera para pasar de pantalla
+        {
+            
+            yield return null;                                 //aqui se coloca el tiempo que se desea que tarde 
+        }
+    }
+
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

407c17c826138d8bc42bafe38f8f7cf5109ff9e3
Hector <hector.bazzinga.dp@gmail.com>
2019-02-27 19:09:18 -0600

    Avance de puzzle 2
    New:Modificacion de menu coneccion con botones y scenas

---
+++ Pantoja_Aguilar_AR/Assets/Scrips/GameControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    private static GameControl instance;
+    public static GameControl Instance { get { return instance; } }
+
+    public GameObject objLoading;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        objLoading = GameObject.Find("Canvas/BachgrunLoading");
+        objLoading.SetActive(false);
+    }
+    
+    private void OnEnable() {
+
+        objLoading=GameObject.Find("Canvas/BachgrunLoading");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadNewScene(int indeexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indeexScene));
+    }
+    IEnumerator LoadScene(int index) {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+
+        while (async != null) {
+            yield return null;
+        }
+    }
+    public void BtnExit(int indeexScene)
+    {
+        Application.Quit();
+    }
+
+
+}

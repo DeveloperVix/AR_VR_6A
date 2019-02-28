bbd46d2e2c5cdca6f5097d9d2624ff53a6809960
JOSE CARLOS MARZANA <car_ma20@yahoo.com.mx>
2019-02-27 19:10:41 -0600

    Avance2_Puzzle
    Se implemento el menu para el juego de puzzle y los comandos para cambiar de menus asi como poder cargar los datos en una pantalla de pausa para que el juego cargue los datos antes y vaya mas fluido

---
+++ ProyectoVuforiaDeteccion/Assets/Scripts/GameControllerUI.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControllerUI : MonoBehaviour
+{
+
+    private static GameControllerUI instance;
+
+    public GameObject objLoading;
+
+    public static GameControllerUI Instance { get { return instance; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else {
+            Debug.LogError("Hay mas de una instancia");
+        }
+
+        objLoading = GameObject.Find("Canvas/backgroundLoading");
+
+        objLoading.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        objLoading = GameObject.Find("Canvas/backgroundLoading");
+        Debug.Log("Estoy activo");
+    }
+
+    #region Buttons UI
+    public void LoadNewScene(int indexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+    public void btnexit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+
+        while (async != null)
+        {
+            yield return null;
+        }
+    }
+    #endregion
+
+}

22bd5a15d7a1e7d1121d2ce3495e9b62eef5afea
Oliver Lara <36975692+OliverLara@users.noreply.github.com>
2019-02-27 19:03:29 -0600

    UI Funcional
    se aplicaron los botones y pantalla de carga de la aplicación

---
+++ AR_Oliver_6to/Assets/Scripts/GameControllerUi.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControllerUi : MonoBehaviour
+{
+    private static GameControllerUi instance;
+
+    public static GameControllerUi Instance { get { return instance; } }
+
+    public GameObject objLoading;
+
+    
+
+    void Start()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        else
+        {
+            Debug.Log("Hay más de una instancia!!");
+        }
+
+        objLoading = GameObject.Find("Canvas/LoadingScene");
+
+        objLoading.SetActive(false);
+    }
+
+    
+
+    #region Buttons UI
+
+    public void LoadNewScene(int indexScene)
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
+    }
+
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+
+        while(async != null)
+        {
+            yield return null;
+        }
+    }
+
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+}

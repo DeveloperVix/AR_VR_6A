2de0cc702d526525fda269e2501ff86e03e95276
BenCarBa <benjas12pumas@hotmail.com>
2019-03-03 17:24:12 -0600

    Tarea_CuboAR
    New Features: En base a un Ray, cuando clickeas la capsula, sirve para hacer una especie de rotacion.
    Agregue el script que hicieron en la semana de GameControllerUI.
    Cree el background de Loading.

---
+++ PracticasAR_6A/Assets/Scripts/ARController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class ARController : MonoBehaviour
+{
+    
+    public Camera cam;
+    public Transform[] waypoints = new Transform[4];
+    public GameObject capsula;
+    public int speed;
+    int currentPoint;
+    public LayerMask layer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Debug.Log("Click");
+            if(Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition),out hit, Mathf.Infinity, layer))
+            {
+                Debug.Log("SegundoIF");
+                for (int i = 0; i < waypoints.Length; i++)
+                {
+                    if(capsula.transform.position == waypoints[currentPoint].position)
+                    {
+                        currentPoint = (currentPoint + 1) % waypoints.Length;
+                    } 
+                }
+            }
+        }
+        capsula.transform.position = Vector3.MoveTowards(capsula.transform.position, waypoints[currentPoint].position, speed * Time.deltaTime);
+
+      
+    }
+}

14e6fd31d3afb74f9cc137960b0717e6b5cefb1d
Victor Gutierrez <36493042+vicgots@users.noreply.github.com>
2019-03-03 23:54:56 -0600

    Tarea_Puzle_AR
    -new features: Se ha agregado al multitarget de la parte front un mini puzle donde el jugador debe mover una pequeña capsula en un plano, obteniendo unas esferas con el fin de hacerlas desaparecer. Una vez obtenidas todas una animación de victoria en forma de saltos se ejecutara
    
    -Bugs: La capsula no ejecuta movimiento al momento de presionar el boton clic izquierdo del raton en el terreno del juego

---
+++ RA-Clase/Assets/Scripts/CapsuleMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Vuforia;
+
+public class CapsuleMovement : MonoBehaviour
+{
+
+
+    DefaultTrackableEventHandler statusImg;
+    protected NavMeshAgent agente;
+    protected Vector3 target;
+
+    GameObject cameraObj;
+    protected Camera camera;
+    public RaycastHit hit;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusImg = GetComponent<DefaultTrackableEventHandler>();
+        agente = gameObject.GetComponent<NavMeshAgent>();
+        cameraObj = GameObject.Find("ARCamera");
+        camera = cameraObj.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(statusImg.isDetected)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Clic izquierdo");
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit))
+                {
+                    target = hit.point;
+                    agente.SetDestination(target);
+                }
+            }
+        }
+        
+        
+    }
+}

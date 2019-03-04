ce2f5ba69d020d3505a11437253092b4eff5f872
Zack098 <37155500+Zack098@users.noreply.github.com>
2019-03-03 20:56:40 -0600

    TareaPuzzle_BaltazarMora
    New Features: se agrego una caja que al darle click muestra un objeto siguiendo waypoints
    Bugs:
    -el raycast no sirve asi que cuando presionan no pasa nada
    -El proyecto original no me dejaba iniciar la camara por un error asi que tuve que trabajar en otro archivo con la misma escena

---
+++ RA_2/Assets/Scripts/RaycastCube.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastCube : MonoBehaviour
+{
+    Camera thisCamera;
+    Camera MainCam;
+    Vector3 mouseInitialPosition;
+    Vector3 mouseFinalPosition;
+    public GameObject[] Waypoints;
+    Rigidbody rb;
+
+    int vel = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCamera = GetComponent<Camera>();
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToWaypoint();
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            //mouseInitialPosition = Camera.current.ScreenToViewportPoint(Input.mousePosition);
+            //mouseInitialPosition = thisCamera.ScreenToViewportPoint(Input.mousePosition);
+            RaycastHit rayHit;
+            //if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity))
+            if (Physics.Raycast(Camera.current.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity))
+            {
+                MoveToWaypoint();
+            }
+
+        }*/
+    }
+
+    public void MoveToWaypoint()
+    {
+        
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            if (rb.transform.position != Waypoints[i].transform.position)
+            {
+                rb.transform.position = Waypoints[i].transform.position;
+            }
+        }
+    }
+}

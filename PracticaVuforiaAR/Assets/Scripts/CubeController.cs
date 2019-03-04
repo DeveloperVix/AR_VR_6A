d4e3a06863a77a14b8a86faa494ee9eefe53e88d
BastianZavala <BastianZavala@users.noreply.github.com>
2019-03-03 23:45:29 -0600

    TareaEventosPuzzle
    Se agregaron 3 eventos que se activan al enfocar el target, permitiendo visualizar un boton y una animacion que al pulsar el boton activa la animacion correspondiente a cada cara

---
+++ PracticaVuforiaAR/Assets/Scripts/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CubeController : MonoBehaviour
+{
+    DefaultTrackableEventHandler statusImg;
+    public GameObject particle;
+    public Animator animatorII;
+    public Animator animatorIII;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusImg = GetComponent<DefaultTrackableEventHandler>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (statusImg.isDetected )
+        {
+            Debug.Log("Inicia Pantalla");
+            PlayButton();
+
+        }
+    }
+
+    public void PlayButton()
+    {
+
+        Instantiate(particle);
+        Instantiate(animatorII);
+        Instantiate(animatorIII);
+    }
+}

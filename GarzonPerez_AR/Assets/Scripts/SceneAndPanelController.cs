fca39cdf371bc41365856f9a44106305606e680b
AbrahamGarzon <thesnowboox@gmail.com>
2019-03-03 19:29:01 -0600

    TareaRaycast
    **Se ah implementado el raycast con un pequeño minijuego (Si tocas el cilindro en el minijuego ganas)**
    **Se arreglo la funcionalidad de los botones**
    **Se cargan escenas y paneles segun la seleccion**

---
+++ GarzonPerez_AR/Assets/Scripts/SceneAndPanelController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneAndPanelController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    public void PanelPlay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Puzzle");
+    }
+}
--- GarzonPerez_AR/Assets/Scripts/VideoScript.cs
+++ GarzonPerez_AR/Assets/Scripts/VideoScript.cs
@@ -9,8 +9,11 @@ public class VideoScript : MonoBehaviour
     DefaultTrackableEventHandler Statusimg;
     public VideoPlayer VideoPlayer;
     bool OnPlays = false;
+    public float range = 10f;
     public Animator anim;
     public Animator animButton;
+    public GameObject cilindro;
+    public GameObject win;
 
     void Start()
     {
@@ -19,13 +22,27 @@ public class VideoScript : MonoBehaviour
     
     private void Update()
     {
-        if(Statusimg.isDetected && !OnPlays){
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(cilindro == true)
+                {
+                    Destroy(cilindro);
+                    win.SetActive(true);
+                }
+            }
+        }
+        if (Statusimg.isDetected && OnPlays){
             playButton();
-            anim.SetBool("Inicio", true);
+            anim.SetBool("Inicio", true);        
             animButton.SetBool("InicioButton", true);
+
         }
         else if (!Statusimg.isDetected)
-        { 
+        {
             pauseButton();
             anim.SetBool("Inicio", false);
             animButton.SetBool("InicioButton", false);

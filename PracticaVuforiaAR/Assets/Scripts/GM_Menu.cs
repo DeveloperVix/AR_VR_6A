6c9600fdcaf4dbe83db0f654a183242ff2880e10
BastianZavala <BastianZavala@users.noreply.github.com>
2019-03-04 19:05:53 -0600

    Ejercicio_Puzzle
    Se agrego un metodo con variables delegadas que permite instanciar y mandar al usuario a las diversas pantallas entre las escenas.

---
+++ PracticaVuforiaAR/Assets/Scripts/GM_Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayButton(int index)
+    {
+        GameControllerUI.Instance.LoadNewScene(index);  //se resetea la referencia al index 
+    }
+
+    public void ExitApp(int index)
+    {
+        //vamo a salirnos ?
+        //ya guardo?
+
+        GameControllerUI.Instance.BtnExit();  //se resetea la referencia al index 
+    }
+}
--- PracticaVuforiaAR/Assets/Scripts/GameControllerUI.cs
+++ PracticaVuforiaAR/Assets/Scripts/GameControllerUI.cs
@@ -20,43 +20,55 @@ public class GameControllerUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (instance == null)
+        if (instance == null)                                                             //la estancia es nula
         {
-            instance = this;
-            DontDestroyOnLoad(this);
+            instance = this;                                                             //instancia igual a este
+            DontDestroyOnLoad(this);                                                     //no destruye el componenete que tiene
         }
         else
         {
-            Debug.LogError("Hay mas de una instancia");
+            Debug.LogError("Hay mas de una instancia");                                  //si hay mas de una isntancia
+            Destroy(gameObject);
         }
 
-        objLoading = GameObject.Find("Canvas/BackgroundLoading");
-
-        objLoading.SetActive(false);
     }
 
-    private void OnEnable()
+    void OnEnable()
     {
-        objLoading = GameObject.Find("Canvas/BackgrounLoading");
-        Debug.Log("Im Here Nigga");
+        Debug.LogError("OnEnable llamado");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        Debug.LogError("OnDisiable llamado");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    //Metodo Propio
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.LogError("Escena cargada");
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");                        //busca la pantalla de loading y la desactiva
+        objLoading.SetActive(false);
     }
 
     #region Buttons UI
 
-    public void LoadNewScene(int indexScene)                    //pediremos que escena quiere que cargue
+    public void LoadNewScene(int indexScene)                                         //pediremos que escena quiere que cargue
     {
         objLoading.SetActive(true);
-        StartCoroutine(LoadScene(indexScene));                    //cambia de escena
+        StartCoroutine(LoadScene(indexScene));                                       //nos pide un parametro entero
     }
 
     IEnumerator LoadScene(int index)
     {
         AsyncOperation async = SceneManager.LoadSceneAsync(index);
 
-        while (async != null)                                   //se indica la cantidad de tiempo de espera para pasar de pantalla
+        while (!async.isDone)                                                        //mientras la operacion asincronica no este terminada retornara nulo
         {
             
-            yield return null;                                 //aqui se coloca el tiempo que se desea que tarde 
+            yield return null;                                                       //aqui se coloca el tiempo que se desea que tarde 
         }
     }
 

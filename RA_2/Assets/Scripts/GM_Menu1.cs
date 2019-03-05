d951fffd6513a06976c9b5e9205971c18979cffb
Zack098 <37155500+Zack098@users.noreply.github.com>
2019-03-04 19:05:17 -0600

    Ejercicio_Puzzle
    New Features: Se agrego la accion para el boton de exit, un boton en la escena del puzzle para regresar al menu

---
+++ RA_2/Assets/Scripts/GM_Menu1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_Menu1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void PlayButton(int index)
+    {
+        GameControllerUI.Instance.LoadNewScene(index);
+    }
+
+    public void ExitApp()
+    {
+        //Seguro que quieres salir
+        //Guardar datos
+        GameControllerUI.Instance.BtnExit();
+    }
+}

211d99f93763359c3558e360e229e2becdb3c145
AbrahamGarzon <thesnowboox@gmail.com>
2019-03-04 19:10:32 -0600

    Ejercicio_Puzzle
    **Modificacion de botones con ccaraga de escena asincrona**
    **Panel de carga**

--- GarzonPerez_AR/Assets/Scripts/SceneAndPanelController.cs
+++ GarzonPerez_AR/Assets/Scripts/SceneAndPanelController.cs
@@ -5,25 +5,71 @@ using UnityEngine.SceneManagement;
 
 public class SceneAndPanelController : MonoBehaviour
 {
+    private static SceneAndPanelController instance;
+    public static SceneAndPanelController Instance { get { return instance; } }
+    public GameObject objLoading;
+
+    public void Game()
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(1));
+    }
+    public void Menu()
+    {
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(0));
+    }
     void Start()
-    {
-
+    { 
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Debug.Log("HolaQuePedoNegro");
+            Destroy(gameObject);
+        }
+        
     }
 
-    void Update()
+    private void OnEnable()
     {
+        Debug.Log("OnEnable llamado!");
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+    private void OnDisable()
+    {
+        Debug.Log("Disable llamado!");
+        SceneManager.sceneLoaded -= OnSceneLoad;
+    }
+    //Metodo Propio
+    void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Escena Cargada");
+        objLoading = GameObject.Find("Canvas/Loading");
+        objLoading.SetActive(false);
+    }
+    #region Buttons UI
 
-    }
-    public void PanelPlay()
+    public void LoadNewScene(int indexScene)
     {
-        SceneManager.LoadScene("Gameplay");
+        objLoading.SetActive(true);
+        StartCoroutine(LoadScene(indexScene));
     }
+    IEnumerator LoadScene(int index)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+    }
+    #endregion
+
     public void ExitGame()
     {
         Application.Quit();
-    }
-    public void MainMenu()
-    {
-        SceneManager.LoadScene("Puzzle");
     }
 }

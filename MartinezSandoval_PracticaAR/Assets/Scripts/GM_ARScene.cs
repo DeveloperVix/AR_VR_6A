c6a61a0a7d5b77deee32155551cbd74e779a45df
Ulikander <>
2019-03-04 19:03:41 -0600

    Ejercicio_Puzzle
    Ejercicio en clase:
    2 game managers nuevos para abrir escenas con botones de manera asincrona.

---
+++ MartinezSandoval_PracticaAR/Assets/Scripts/GM_ARScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM_ARScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GoMenu(string scene)
+    {
+        GameController.act.LoadNewScene(scene);
+    }
+}
--- MartinezSandoval_PracticaAR/Assets/Scripts/GameController.cs
+++ MartinezSandoval_PracticaAR/Assets/Scripts/GameController.cs
@@ -5,25 +5,69 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public static GameController act;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (act != null)
+        {
+            Destroy(gameObject);
+        }
+
+        act = this;
+        DontDestroyOnLoad(this);
+
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    private void OnEnable()
+    {
+        Debug.LogError("Un saludote desde OnEnable()");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        Debug.LogError("Un saludote desde OnDisable()");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    //Metodo propio
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.LogError("Escena cargada");
+    }
+
+    #region Buttons UI
 
     public void ButtonPlay()
     {
-        StartCoroutine(LoadScene("PuzzleGame"));
+        LoadNewScene("PuzzleGame");
+    }
+
+    public void LoadNewScene(string scene)
+    {
+        StartCoroutine(LoadScene(scene));
     }
 
     IEnumerator LoadScene(string scene)
-    {
+    {                               
         AsyncOperation load = SceneManager.LoadSceneAsync(scene);
         yield return load.isDone;
         load.allowSceneActivation = true;
     }
+
+    public void ButtonExit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
 }

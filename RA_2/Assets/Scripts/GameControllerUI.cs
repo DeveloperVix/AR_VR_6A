c7020157567eaf54c1af82bf2154e905374c6e04
Zack098 <37155500+Zack098@users.noreply.github.com>
2019-03-04 19:03:07 -0600

    Revert "Avanze_Puzzle_3"
    This reverts commit 54f76f10580177cbdc38d2801458c698fc0bd582.

--- RA_2/Assets/Scripts/GameControllerUI.cs
+++ RA_2/Assets/Scripts/GameControllerUI.cs
@@ -23,34 +23,15 @@ public class GameControllerUI : MonoBehaviour
         else
         {
             Debug.LogError("Hay más de una instancia!");
-            Destroy(gameObject);
         }
-    }
-
-    /*private void Enable()
-    {
-        //objLoading = GameObject.Find("Canvas/BackgroundLoading");
-        Debug.Log("Estoy Activo");
-    }*/
-
-    void OnEnable()
-    {
-        Debug.LogError("OnEnable llamado");
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    void OnDisable()
-    {
-        Debug.LogError("OnDisable llamado");
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    //metodo propio
-    void OnSceneLoaded(Scene scene,LoadSceneMode mode)
-    {
-        Debug.LogError("Escena cargada");
         objLoading = GameObject.Find("Canvas/BackgroundLoading");
         objLoading.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        objLoading = GameObject.Find("Canvas/BackgroundLoading");
+        Debug.Log("Estoy Activo");
     }
 
     #region Buttons UI
@@ -65,7 +46,7 @@ public class GameControllerUI : MonoBehaviour
     {
         AsyncOperation async = SceneManager.LoadSceneAsync(index);////cargar la escena de forma asincronica
 
-        while (!async.isDone)//mientras la operacion asincronica no este terminada retornada null
+        while (async != null)
         {
             yield return null;
         }

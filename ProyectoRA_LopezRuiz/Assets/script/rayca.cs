21633b0abb13656093af782bf534b8e1004bfed0
vicsay1 <36805512+vicsay1@users.noreply.github.com>
2019-03-06 19:19:05 -0600

    Ejercicio_Puzzle2
    Se añadio un puzzle mas a el cubo en el que se tiene que girar el triangulo hasta que sale volando de la pantalla.

--- ProyectoRA_LopezRuiz/Assets/script/rayca.cs
+++ ProyectoRA_LopezRuiz/Assets/script/rayca.cs
@@ -6,13 +6,30 @@ using UnityEngine.UI;
 using TMPro;
 
 public class rayca : MonoBehaviour {
-
+    //puzzle colores
     public float raylength;
-    public LayerMask layermask, layermask1, layermask2, layermask3;
+    public LayerMask layermask, layermask1, layermask2, layermask3, layermask4;
     bool ama = true, verd = true, azu= true, rojo= true;
     public Material color_actual;
+    //puzzle colores
 
+
+
+    //puzzle triangulo
+    int speed = 0;
+    Coroutine speedCo;
+    public Animator triangulo_anim;
+    public Transform target;
+    //puzzle triangulo
+
+
+
+    //general
     public TextMeshProUGUI texto_puzzles;
+    int puzzles_superados;
+    //general
+    
+
 	void Start ()
     {
         color_actual.color = Color.yellow;
@@ -22,13 +39,22 @@ public class rayca : MonoBehaviour {
 	
 	void Update ()
     {
+        raycast_funcion();
+        puzzle_triangulo();
+    }
+
+
+    public void raycast_funcion()
+    {
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray,out hit, raylength, layermask))
+
+            //cuadrados de colores
+            if (Physics.Raycast(ray, out hit, raylength, layermask))
             {
-                if (hit.collider.name == "amarillo" && ama == true)
+                if (ama == true)
                 {
                     Debug.Log("lol si entra solo al cubo");
                     color_actual.color = Color.red;
@@ -37,7 +63,7 @@ public class rayca : MonoBehaviour {
             }
             if (Physics.Raycast(ray, out hit, raylength, layermask1))
             {
-                if (hit.collider.name == "rojo" && rojo == true)
+                if (rojo == true)
                 {
                     Debug.Log("rojo");
                     color_actual.color = Color.blue;
@@ -46,7 +72,7 @@ public class rayca : MonoBehaviour {
             }
             if (Physics.Raycast(ray, out hit, raylength, layermask2))
             {
-                if (hit.collider.name == "azul" && azu == true)
+                if (azu == true)
                 {
                     Debug.Log("azul");
                     color_actual.color = Color.green;
@@ -55,14 +81,57 @@ public class rayca : MonoBehaviour {
             }
             if (Physics.Raycast(ray, out hit, raylength, layermask3))
             {
-                if (hit.collider.name == "verde" && verd == true)
+                if (verd == true)
                 {
                     Debug.Log("verde");
                     color_actual.color = Color.white;
                     verd = false;
-                    texto_puzzles.text = "puzzles 1";
+                    puzzles_superados += 1;
+                    texto_puzzles.text = "puzzles " + puzzles_superados;
+
+                }
+            }
+
+            //triangulo
+            if (Physics.Raycast(ray, out hit, raylength, layermask4))
+            {
+                speed += 2;
+                if (speedCo == null)
+                {
+                    print("Entre jeje");
+                    speedCo = StartCoroutine("ralientizar");
+                }
+                if (speed > 30)
+                {
+                    triangulo_anim.SetBool("vuela",true);
+                    puzzles_superados += 1;
+                    texto_puzzles.text = "puzzles " + puzzles_superados;
                 }
             }
         }
-	}
+    }
+
+    
+    public void puzzle_triangulo()
+    {
+        target.transform.Rotate(0, 0, speed);
+    }
+    
+
+    IEnumerator ralientizar()
+    {
+        bool hazlo = true;
+        while (hazlo)
+        {
+            yield return new WaitForSeconds(0.5f);
+            speed -= 1;
+            if (speed <= 0)
+            {
+                speed = 0;
+                hazlo = false;
+            }
+        }
+        speedCo = null;
+    }
 }
+//comit del domingo (puzzle_final) y el de hoy ejercicio puzzle

584edac1986b2cf9519255f503624775d14a02e0
Miguel-SilvaxDD <42754121+Miguel-SilvaxDD@users.noreply.github.com>
2019-03-08 20:15:44 -0600

    Tarea_Puzzle_Parte2
    New Features: Se implementaron dos scripts, uno para el comportamiento de los puzzles y otro para el raycast, se implementaron todos los comportamientos de los puzzles.
    
    Bugs: Aun no se puede ganar

---
+++ ARproyect/Assets/scripts/Puzzles.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzles : MonoBehaviour
+{
+   // public Animator Escala11;
+
+    [SerializeField]
+    Transform[] waypoints;
+
+    [SerializeField]
+    float moveSpeed = 2f;                                                  
+    int waypointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public virtual void Movimiento()                                                 
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime); 
+        if (transform.position == waypoints[waypointIndex].transform.position)  
+        {
+            waypointIndex += 1;                                               
+        }
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    public virtual void Movimiento2()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    public virtual void Movimiento3()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[waypointIndex].transform.position, moveSpeed * Time.deltaTime);
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+        if (waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+    /* public void Escala1()
+     {
+         Escala11.SetBool("entrada", true);
+     }*/
+}
---
+++ ARproyect/Assets/scripts/Raycast.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+
+    [SerializeField]
+    public Camera thisCamera;
+    public Animator Anim;
+    public Animator Anim2;
+    public Animator Anim3;
+    public Puzzles puzzle;
+    public Puzzles puzzle2;
+    public Puzzles puzzle3;
+    public LayerMask chucha;
+    public LayerMask chucha2;
+    public LayerMask chucha3;
+    public LayerMask chucha4;
+    public LayerMask chucha5;
+    public LayerMask chucha6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clic izq");
+            RaycastHit rayHit;
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, chucha))
+            {
+                puzzle.Movimiento();
+                Debug.Log("Tocaste el objeto 1 xd");
+            }
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, chucha2))
+            {
+                puzzle2.Movimiento2();
+                Debug.Log("Tocaste el objeto 2 xd");
+            }
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, chucha3))
+            {
+                //puzzle.Escala1();
+                Anim.SetBool("salida", true);
+                Debug.Log("Tocaste el objeto 3 xd"); 
+            }
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, chucha4))
+            {
+                puzzle3.Movimiento3();
+                Debug.Log("Tocaste el objeto 4 xd");
+            }
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, chucha5))
+            {
+                //puzzle.Escala1();
+                Anim2.SetBool("Salida2", true);
+                Debug.Log("Tocaste el objeto 3 xd");
+            }
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, chucha6))
+            {
+                //puzzle.Escala1();
+                Anim3.SetBool("DOS", true);
+                Debug.Log("Tocaste el objeto 3 xd");
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit rayHit1;
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit1, Mathf.Infinity, chucha3))
+            {
+                //puzzle.Escala1();
+                Anim.SetBool("salida", false);
+                Debug.Log("Tocaste la segunda");
+            }
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit1, Mathf.Infinity, chucha5))
+            {
+                //puzzle.Escala1();
+                Anim2.SetBool("Salida2", false);
+                Debug.Log("Tocaste la segunda");
+            }
+            if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit1, Mathf.Infinity, chucha6))
+            {
+                //puzzle.Escala1();
+                Anim3.SetBool("DOS", false);
+                Debug.Log("Tocaste la segunda");
+            }
+        }
+    }
+
+
+
+    
+}

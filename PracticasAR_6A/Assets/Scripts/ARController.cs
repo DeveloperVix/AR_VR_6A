e7e23581ededb6380d388688c0ea447eb43ebb03
BenCarBa <benjas12pumas@hotmail.com>
2019-03-09 01:48:13 -0600

    Puzzle_Parte2
    New Features: Integracion Minijuego diferencias, ya funciona perfectamente la logica.
    
    Bugs: No se como poner cuantas has encontrado, y el mesh no me funciono, no se si se puede asignarle un mesh a un vacio desde codigo

--- PracticasAR_6A/Assets/Scripts/ARController.cs
+++ PracticasAR_6A/Assets/Scripts/ARController.cs
@@ -23,11 +23,16 @@ public class ARController : MonoBehaviour, ITrackableEventHandler
     public int speed;
     int currentPoint;
     public GameObject Botones;
+    public GameObject TitulosQR;
+    public GameObject ImagenesDiferencias;
+    public Transform CuboPuntero; 
     /*public GameObject minijuegoMariposa;
     public GameObject minijuegoDonald;
     public GameObject minijuegoMickey;
 */
-    float valor;
+    int valor;
+    int _valor;
+    string op;
 
     Ray ray;
     RaycastHit rayHit;
@@ -43,7 +48,8 @@ public class ARController : MonoBehaviour, ITrackableEventHandler
             mTrackableBehaviour.RegisterTrackableEventHandler(this);
 
         Botones.SetActive(false);
-        
+        TitulosQR.SetActive(false);
+        ImagenesDiferencias.SetActive(false);
     }
 
     // Update is called once per frame
@@ -76,7 +82,9 @@ public class ARController : MonoBehaviour, ITrackableEventHandler
             if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, LayerMask.GetMask("CaraQR")))
             {
                 statusPuzzle = estadosPuzzle.CaraQR;
-
+                TitulosQR.SetActive(true);
+                ImagenesDiferencias.SetActive(true);
+                Diferencias(op);
                 Debug.Log("CaraQR");
             }
 
@@ -98,7 +106,9 @@ public class ARController : MonoBehaviour, ITrackableEventHandler
                 statusPuzzle = estadosPuzzle.CaraDonald;
                 //minijuegoDonald.SetActive(true);
                 Botones.SetActive(true);
-                MinijuegoDonald(valor);
+                // MinijuegoDonald(valor);
+                Aumento(valor);
+                Decremento(_valor);
             }
 
             if (Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, LayerMask.GetMask("CaraMariposa")))
@@ -111,19 +121,82 @@ public class ARController : MonoBehaviour, ITrackableEventHandler
         }
     }
 
-    public void MinijuegoDonald(float _valor)
+  /*  public void MinijuegoDonald(float _valor)
     {
         
         GameObject cubo = GameObject.Find("CuboAzul");
         Vector3 cuboScale = cubo.transform.localScale;
-        cuboScale.x += _valor;
-        cuboScale.y += _valor;
-        cuboScale.z += _valor;
+        if (_valor > 0)
+        {
+            Debug.Log("Mas");
+            cuboScale.x += _valor;
+            cuboScale.y += _valor;
+            cuboScale.z += _valor;
+        }
+        else
+        {
+            Debug.Log("MENOS");
+            cuboScale.x -= _valor;
+            cuboScale.y -= _valor;
+            cuboScale.z -= _valor;
+        }
+        cubo.transform.localScale = cuboScale;
+    }
+    */
+    public void Aumento(int valor)
+    {
+        Debug.Log("MAs");
+        GameObject cubo = GameObject.Find("CuboAzul");
+         Vector3 cuboScale = cubo.transform.localScale;
+        cuboScale.x += valor;
+        cuboScale.y += valor;
+        cuboScale.z += valor;
+        cubo.transform.localScale = cuboScale;
+    }
+    public void Decremento(int _valor)
+    {
+        Debug.Log("Menos");
+        GameObject cubo = GameObject.Find("CuboAzul");
+        Vector3 cuboScale = cubo.transform.localScale;
+        cuboScale.x -= _valor;
+        cuboScale.y -= _valor;
+        cuboScale.z -= _valor;
         cubo.transform.localScale = cuboScale;
     }
 
-  
+    public void Diferencias(string op)
+    {
+        Vector3 cuboTranslacion = CuboPuntero.transform.position;
+        switch (op)
+        {
+            case "up":
+                cuboTranslacion.x += 0;
+                cuboTranslacion.y += 0;
+                cuboTranslacion.z += .1f;
+                break;
+            case "down":
+                cuboTranslacion.x += 0;
+                cuboTranslacion.y += -0;
+                cuboTranslacion.z += -.1f;
+                break;
+            case "left":
+                cuboTranslacion.x += -.1f;
+                cuboTranslacion.y += 0;
+                cuboTranslacion.z += 0;
+                break;
+            case "right":
+                cuboTranslacion.x += .1f;
+                cuboTranslacion.y += 0;
+                cuboTranslacion.z += 0;
+                break;
+            default:
+                return;
+              
+        }
+        CuboPuntero.transform.position = cuboTranslacion;
+    }
 
+    
 
     public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
     {
@@ -139,19 +212,27 @@ public class ARController : MonoBehaviour, ITrackableEventHandler
                 Botones.SetActive(true);
                 //minijuegoDonald.SetActive(true);
             }
-           
+            if (statusPuzzle == estadosPuzzle.CaraQR)
+            {
+                TitulosQR.SetActive(true);
+                ImagenesDiferencias.SetActive(true);
+            }
+
         }
         else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
                  newStatus == TrackableBehaviour.Status.NO_POSE)
         {
-           // Botones.SetActive(false);
+            Botones.SetActive(false);
             //minijuegoMariposa.SetActive(false);
             //minijuegoMickey.SetActive(false);
+            TitulosQR.SetActive(false);
+            ImagenesDiferencias.SetActive(false);
         }
         else
         {
             Botones.SetActive(false);
-            
+            TitulosQR.SetActive(false);
+            ImagenesDiferencias.SetActive(false);
         }
     }
 }

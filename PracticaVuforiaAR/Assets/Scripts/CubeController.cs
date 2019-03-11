f2b99bad149d0069e177e35764749812baa4fb1b
BastianZavala <BastianZavala@users.noreply.github.com>
2019-03-10 23:26:23 -0600

    Puzzle_Final
    Se agrego un plano para cada cara que es el tablero del jugador
    Se agrego un puzzle de waypoints entre un obstaculo moviendose
    Se agrego un puzzle de burbujas cuyo proposito es reventarlas
    Se agrego un puzzle de Movimiento donde se tiene que esquivar
    Se agrego un puzzle de escalacion para una figura
    Bugs:
    Las burbujas se instancian en la direccion equivocada y por montones.
    El raycast de escalacion no detecta bien la figura con la que colisiona
    La figura de waypoints tiene poca o nula velocidad

--- PracticaVuforiaAR/Assets/Scripts/CubeController.cs
+++ PracticaVuforiaAR/Assets/Scripts/CubeController.cs
@@ -5,33 +5,84 @@ using Vuforia;
 
 public class CubeController : MonoBehaviour
 {
-    DefaultTrackableEventHandler statusImg;
-    public GameObject particle;
-    public Animator animatorII;
-    public Animator animatorIII;
+
+    public int indexWayPoints = 0;
+    public Transform[] points = new Transform[0];
+    public int speed;
+    public GameObject cubo;
+
+    public Camera gopro;
+    public GameObject herramienta;
+    public Animator escala;
+
+    public GameObject burbuja;
+    int contadorBurbujas;
+    public LayerMask layer;
+    RaycastHit ray;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        statusImg = GetComponent<DefaultTrackableEventHandler>();
+       
     }
+
 
     // Update is called once per frame
     private void Update()
     {
-        if (statusImg.isDetected )
-        {
-            Debug.Log("Inicia Pantalla");
-            PlayButton();
+        Movimiento();
+        StartCoroutine("spawnBubles");
+    }
 
+    #region Metodos
+    public virtual void Movimiento()
+    {
+        cubo.transform.position = Vector3.MoveTowards(transform.position, points[indexWayPoints].transform.position, speed * Time.deltaTime);
+        if (cubo.transform.position == points[indexWayPoints].position)
+        {
+            indexWayPoints++;
+        }
+        if (indexWayPoints == points.Length)
+        {
+            indexWayPoints = 0;
         }
     }
 
-    public void PlayButton()
+    public void Escalar()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = gopro.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                escala.SetBool("Escalando", false);
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = gopro.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                escala.SetBool("Escalando", true);
+            }
+        }
+    }
+
+
+    #endregion Metodos
+
+    #region Cortinas
+    IEnumerator spawnBubles()
     {
 
-        Instantiate(particle);
-        Instantiate(animatorII);
-        Instantiate(animatorIII);
+        Instantiate(burbuja);
+        yield return new WaitForSeconds(5f);
+        Destroy(this);
+        
     }
+    #endregion Cortinas
+
 }

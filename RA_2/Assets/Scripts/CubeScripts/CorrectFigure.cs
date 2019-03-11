0340734d78d092acad2be948bc3d57e4996a8add
Zack098 <37155500+Zack098@users.noreply.github.com>
2019-03-10 23:29:15 -0600

    Puzzle_Final
    New Features: se agrego que los puzzle dieran puntos al jugador por completarlos correctamente y un boton de reinicio
    Bugs: solo 2 de los puzzle otorgan bien puntos al jugador pero no hay un texto que muestre el total de puntos actuales del jugador

---
+++ RA_2/Assets/Scripts/CubeScripts/CorrectFigure.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorrectFigure : MonoBehaviour
+{
+    [Header("Puntos obtenibles de la prueba")]
+    public int Points = 0;
+    public int puntosObtenibles = 1;
+    [Header("Figuras de")]
+    public GameObject Figura;
+    public GameObject FiguraPrincipal;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (Figura.tag == FiguraPrincipal.tag)
+            {
+
+                if (puntosObtenibles == 1)
+                {
+                    Debug.Log("Figura correcta!");
+                    puntosObtenibles -= 1;
+                    Debug.Log("Obtuviste un punto de la prueba");
+                    Points += 1;
+                }
+
+                if (puntosObtenibles <= 0)
+                {
+                    puntosObtenibles = 0;
+                }
+                if (Points >= 1)
+                {
+                    Points = 1;
+                }
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ RA_2/Assets/Scripts/CubeScripts/unoCards.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unoCards : MonoBehaviour
+{
+    [Header("Total de veces a clickear")]
+    public int vida = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        vida -= 1;
+        if (vida <= 0)
+        {
+            vida = 0;
+        }
+    }
+}

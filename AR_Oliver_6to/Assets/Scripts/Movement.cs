0f8e7a234418bb6202d6e448fbfa59a4711aa886
Oliver Lara <36975692+OliverLara@users.noreply.github.com>
2019-03-10 23:24:41 -0600

    Puzzle_Final
    Se agregaron 5 puzzles distintos, traslación y rotación fueron las mecanicas principales.

--- AR_Oliver_6to/Assets/Scripts/Movement.cs
+++ AR_Oliver_6to/Assets/Scripts/Movement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 0.4f;
+
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform respawnpoint;
+
+    public float speed = 0.1f;
 
     private Vector3 direction = Vector3.zero;
 
@@ -24,6 +28,129 @@ public class Movement : MonoBehaviour
         vel.x = h;
         vel.z = v;
         rb.velocity = vel;
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "col1")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col2")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col3")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+           Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col4")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col5")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col6")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col7")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col8")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+           Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col9")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col10")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+           Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col11")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col12")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col13")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col14")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col15")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "col16")
+        {
+            Debug.Log("Colision detectada");
+            Destroy(gameObject);
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
+        else if (col.gameObject.name == "Meta")
+        {
+            Debug.Log("Colision detectada");
+            
+            Player.transform.position = respawnpoint.transform.position;
+        }
+
     }
 
 }
---
+++ AR_Oliver_6to/Assets/Scripts/respawnPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawnPoint : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform respawnpoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position = respawnpoint.transform.position;
+    }
+}

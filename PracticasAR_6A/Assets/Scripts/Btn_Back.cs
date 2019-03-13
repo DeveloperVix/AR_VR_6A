4751f36927e4c55ac8823e69fae51fd2a47e1611
BenCarBa <benjas12pumas@hotmail.com>
2019-03-13 11:36:53 -0600

    Puzzle_Final
    New Features:
    -Boton de regreso despues de cada pantalla de win.
    -Avisos de lo que tienes que hacer en los puzzles mas complejos

---
+++ PracticasAR_6A/Assets/Scripts/Btn_Back.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Btn_Back : MonoBehaviour
+{
+    public GameObject win;
+    public GameObject boton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (win.activeInHierarchy)
+        {
+            boton.SetActive(true);
+        }
+        else
+        {
+            boton.SetActive(false);
+        }
+    }
+    public void OnClick()
+    {
+        win.SetActive(false);
+    }
+}

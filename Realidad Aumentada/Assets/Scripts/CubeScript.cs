2e48dcf2d98e5fb9f03bd761f41547f0e316172b
LeonardoCV1998 <Leocastrovalladolid@hotmail.com>
2019-03-13 16:05:31 -0600

    Arreglo_Examen
    Se modifico para agregar la interfaz para el usuario y su experienca conforme a la UI

--- Realidad Aumentada/Assets/Scripts/CubeScript.cs
+++ Realidad Aumentada/Assets/Scripts/CubeScript.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
-    //OBJETOS A UTILIZAR
+    public static CubeScript act;
 
+    //OBJETOS A UTILIZAR
     public GameObject sphere;
     public GameObject sphere2;
     public GameObject sphere3;
     public GameObject redCubeObj;
     public GameObject greenCubeObj;
     public GameObject greenCube2Obj;
+    public GameObject notFoundTarget;
+    public GameObject puzzle1;
+    public GameObject puzzle2;
+    public GameObject puzzle3;
+    public GameObject puzzle4;
+    public GameObject puzzle5;
+    public GameObject puzzle6;
+    public GameObject instructions;
 
     //LAYERS DE OBJETOS
     public LayerMask cubeSelected;
@@ -22,6 +31,12 @@ public class CubeScript : MonoBehaviour
     public LayerMask redCubeSelected;
     public LayerMask greenCubeSelected;
     public LayerMask yellowCubeSelected;
+    public LayerMask puzzle1Layer;
+    public LayerMask puzzle2Layer;
+    public LayerMask puzzle3Layer;
+    public LayerMask puzzle4Layer;
+    public LayerMask puzzle5Layer;
+    public LayerMask puzzle6Layer;
 
     //MATERIAL
     public Material redCube;
@@ -43,10 +58,16 @@ public class CubeScript : MonoBehaviour
 
     void Awake()
     {
-        
+        act = this;
         thisCamera = GetComponent<Camera>();                         //Accedemos a los componentes
         statusAnim.GetComponent<DefaultTrackableEventHandler>();     //Obtenemos los componentes de DefaultTrackableEventHandler
-
+        puzzle1.SetActive(false);
+        puzzle2.SetActive(false);
+        puzzle3.SetActive(false);
+        puzzle4.SetActive(false);
+        puzzle5.SetActive(false);
+        puzzle6.SetActive(false);
+        instructions.SetActive(false);
 
     }
 
@@ -54,6 +75,8 @@ public class CubeScript : MonoBehaviour
     {
         if (statusAnim.isDetected == true)                                                                                           //Si se detecto el target
         {
+            notFoundTarget.SetActive(false);
+            instructions.SetActive(true);
             if (Input.GetMouseButtonDown(0))                                                                                         //Si ha sido presionado el click izquierdo
             {
                 mouseInitialPosition = thisCamera.ScreenToViewportPoint(Input.mousePosition);                                        //Transforma las coordenadas de pantalla en un espacio de viewport(nos retorna coordenadas de 0 a 1)
@@ -62,73 +85,122 @@ public class CubeScript : MonoBehaviour
 
 
                 //PUZZLE 1
-                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, sphereSelected))   //Si el click del mouse colisiona con el layer de sphereSelected...
+                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, puzzle1Layer))
                 {
-                    Vector3 scaleSphere = sphere.transform.localScale;                                                               //Se crea un vector 3 que guardara la escala de la sphere
-                    scaleSphere.x += 0.2f;                                                                                           //Se le agrega en x 0.2 cada vez que pase por aqui
-                    scaleSphere.y += 0.2f;                                                                                           //Se le agrega en y 0.2 cada vez que pase por aqui
-                    scaleSphere.z += 0.2f;                                                                                           //Se le agrega en z 0.2 cada vez que pase por aqui
-                    sphere.transform.localScale = scaleSphere;                                                                       //Se le agregara la variable del vector 3 y se le asignara a la escala de la sphere
+                    puzzle1.SetActive(true);
+                    GameController.act.backgroundPuzzle1.SetActive(true);
+                    //GameController.act.puzzle1.SetActive(false);
+                    if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, sphereSelected))   //Si el click del mouse colisiona con el layer de sphereSelected...
+                    {
+                        Vector3 scaleSphere = sphere.transform.localScale;                                                               //Se crea un vector 3 que guardara la escala de la sphere
+                        scaleSphere.x += 0.2f;                                                                                           //Se le agrega en x 0.2 cada vez que pase por aqui
+                        scaleSphere.y += 0.2f;                                                                                           //Se le agrega en y 0.2 cada vez que pase por aqui
+                        scaleSphere.z += 0.2f;                                                                                           //Se le agrega en z 0.2 cada vez que pase por aqui
+                        sphere.transform.localScale = scaleSphere;                                                                       //Se le agregara la variable del vector 3 y se le asignara a la escala de la sphere
+                    }
                 }
+
 
 
                 //PUZZLE 2
-                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, cubeSelected))     //Si el click del mouse colisiona con el layer de cubeSelected...
+                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, puzzle2Layer))
                 {
-                    meshCube.material = redCube;                                                                                     //Se le agrega el materiaul de redCube al cubo
-                    anim.SetBool("animActive", false);                                                                               //La animacion se detiene
-                    GameController.act.Puzzle3();                                                                                    //Inicial el metodo llamado Puzzle3 del GameController
-                    
+                    puzzle2.SetActive(true);
+                    GameController.act.backgroundPuzzle2.SetActive(true);
+                    if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, cubeSelected))     //Si el click del mouse colisiona con el layer de cubeSelected...
+                    {
+                        meshCube.material = redCube;                                                                                     //Se le agrega el materiaul de redCube al cubo
+                        anim.SetBool("animActive", false);                                                                               //La animacion se detiene
+                        GameController.act.Puzzle3();                                                                                    //Inicial el metodo llamado Puzzle3 del GameController
+
+                    }
                 }
+
 
 
                 //PUZZLE 3
-                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, sphereSelected2))  //Si el click del mouse colisiona con el layer de sphereSelected2...
+                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, puzzle3Layer))
                 {
-                    sphere2.transform.Rotate(0, sphere2.transform.rotation.y + 10, 0);                                               //Se le agregara en su rotacion en y + 10
+                    puzzle3.SetActive(true);
+                    GameController.act.backgroundPuzzle3.SetActive(true);
+                    if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, sphereSelected2))  //Si el click del mouse colisiona con el layer de sphereSelected2...
+                    {
+                        sphere2.transform.Rotate(0, sphere2.transform.rotation.y + 10, 0);                                               //Se le agregara en su rotacion en y + 10
+                    }
+
+                    if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, sphereSelected3))  //Si el click del mouse colisiona con el layer de sphereSelected3...
+                    {
+                        sphere3.transform.Rotate(0, sphere3.transform.rotation.y - 10, 0);                                               //Se le agregara en su rotacion en y - 10
+                    }
                 }
 
-                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, sphereSelected3))  //Si el click del mouse colisiona con el layer de sphereSelected3...
+
+
+                //PUZZLE 4
+                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, puzzle4Layer))
                 {
-                    sphere3.transform.Rotate(0, sphere3.transform.rotation.y - 10, 0);                                               //Se le agregara en su rotacion en y - 10
+                    GameController.act.backgroundPuzzle4.SetActive(true);
+                    puzzle4.SetActive(true);
+                    GameController.act.wasdTxtRedCube.SetActive(true);
                 }
 
+
+
+                //PUZZLE 5
+                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, puzzle5Layer))
+                {
+                    GameController.act.backgroundPuzzle5.SetActive(true);
+                    puzzle5.SetActive(true);
+                    GameController.act.wasdTxtSphere.SetActive(true);
+
+                }
 
 
                 //PUZZLE 6
-                if(GameController.act.redImage.activeInHierarchy)
+                if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, puzzle6Layer))
                 {
-                    if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, redCubeSelected))  //Si el click del mouse colisiona con el layer de sphereSelected2...
+                    puzzle6.SetActive(true);
+                    GameController.act.backgroundPuzzle6.SetActive(true);
+                    GameController.act.backgroundColor.SetActive(true);
+                    int i = 0;
+                    if(i <= 0)
                     {
-                        GameController.act.Finish();
+                        GameController.act.AparicionImage();
+                    }
+                    i++;
+                    
+                    if (GameController.act.redImage.activeInHierarchy)
+                    {
+                        if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, redCubeSelected))  //Si el click del mouse colisiona con el layer de sphereSelected2...
+                        {
+                            GameController.act.Finish();
+                        }
+                    }
+
+                    if (GameController.act.greenImage.activeInHierarchy)
+                    {
+                        if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, greenCubeSelected))  //Si el click del mouse colisiona con el layer de sphereSelected2...
+                        {
+                            GameController.act.Finish();
+                        }
+                    }
+
+                    if (GameController.act.yellowImage.activeInHierarchy)
+                    {
+                        if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, yellowCubeSelected))  //Si el click del mouse colisiona con el layer de sphereSelected2...
+                        {
+                            GameController.act.Finish();
+                        }
                     }
                 }
-
-                if (GameController.act.greenImage.activeInHierarchy)
-                {
-                    if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, greenCubeSelected))  //Si el click del mouse colisiona con el layer de sphereSelected2...
-                    {
-                        GameController.act.Finish();
-                    }
-                }
-
-                if (GameController.act.yellowImage.activeInHierarchy)
-                {
-                    if (Physics.Raycast(thisCamera.ScreenPointToRay(Input.mousePosition), out rayHit, Mathf.Infinity, yellowCubeSelected))  //Si el click del mouse colisiona con el layer de sphereSelected2...
-                    {
-                        GameController.act.Finish();
-                    }
-                }
-
-
             }
         }
         else
         {
             if (statusAnim.isDetected == false)
             {
-                anim.SetBool("animActive", false);
-
+                //anim.SetBool("animActive", false);
+                notFoundTarget.SetActive(true);
             }
         }
         
--- Realidad Aumentada/Assets/Scripts/GameController.cs
+++ Realidad Aumentada/Assets/Scripts/GameController.cs
@@ -26,12 +26,19 @@ public class GameController : MonoBehaviour
     public GameObject greenImage;
     public GameObject yellowImage;
     public GameObject backgroundColor;
+    public GameObject backgroundPuzzle1;
+    public GameObject backgroundPuzzle2;
+    public GameObject backgroundPuzzle3;
+    public GameObject backgroundPuzzle4;
+    public GameObject backgroundPuzzle5;
+    public GameObject backgroundPuzzle6;
 
 
 
     void Start()
     {
         act = this;                             //El constructor sera este codigo
+
         buscaTxt1.SetActive(false);             //Se desaparece el texto de buscar
         wasdTxtRedCube.SetActive(false);
         wasdTxtSphere.SetActive(false);
@@ -45,8 +52,13 @@ public class GameController : MonoBehaviour
         yellowImage.SetActive(false);
         finishBackground.SetActive(false);
         backgroundColor.SetActive(false);
+        backgroundPuzzle1.SetActive(false);
+        backgroundPuzzle2.SetActive(false);
+        backgroundPuzzle3.SetActive(false);
+        backgroundPuzzle4.SetActive(false);
+        backgroundPuzzle5.SetActive(false);
+        backgroundPuzzle6.SetActive(false);
     }
-
 
 
     //PUZZLE 2
@@ -61,7 +73,11 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(1f);    //Se espera un segundo
         buscaTxt1.SetActive(false);             //Desactiva el texto de buscar
         puzzle1.SetActive(false);               //Desactiva el puzzle 1
+        CubeScript.act.puzzle1.SetActive(false);
+        backgroundPuzzle1.SetActive(false);
         puzzle2.SetActive(true);                //Activa el puzzle 2
+
+        
     }
 
 
@@ -78,7 +94,10 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(1f);    //Se espera un segundo
         buscaTxt1.SetActive(false);             //Desactiva el texto de buscar
         puzzle2.SetActive(false);               //Desactiva el puzzle 2
+        CubeScript.act.puzzle2.SetActive(false);
+        backgroundPuzzle2.SetActive(false);
         puzzle3.SetActive(true);                //Activa el puzzle 3
+       
     }
 
 
@@ -95,8 +114,10 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(1f);    //Se espera un segundo
         buscaTxt1.SetActive(false);             //Desactiva el texto de buscar
         puzzle3.SetActive(false);               //Desactiva el puzzle 2
+        CubeScript.act.puzzle3.SetActive(false);
+        backgroundPuzzle3.SetActive(false);
         puzzle4.SetActive(true);                //Activa el puzzle 3
-        wasdTxtRedCube.SetActive(true);
+        
     }
 
 
@@ -113,9 +134,10 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(1f);    //Se espera un segundo
         buscaTxt1.SetActive(false);             //Desactiva el texto de buscar
         puzzle4.SetActive(false);               //Desactiva el puzzle 2
+        CubeScript.act.puzzle4.SetActive(false);
         wasdTxtRedCube.SetActive(false);
+        backgroundPuzzle4.SetActive(false);
         puzzle5.SetActive(true);                //Activa el puzzle 3
-        wasdTxtSphere.SetActive(true);
     }
 
 
@@ -132,10 +154,12 @@ public class GameController : MonoBehaviour
         yield return new WaitForSeconds(1f);    //Se espera un segundo
         buscaTxt1.SetActive(false);             //Desactiva el texto de buscar
         puzzle5.SetActive(false);               //Desactiva el puzzle 2
+        CubeScript.act.puzzle5.SetActive(false);
         wasdTxtSphere.SetActive(false);
+        backgroundPuzzle5.SetActive(false);
         puzzle6.SetActive(true);                //Activa el puzzle 3
-        backgroundColor.SetActive(true);
-        AparicionImage();
+        
+        
     }
 
 
@@ -144,6 +168,7 @@ public class GameController : MonoBehaviour
     public void Finish()
     {
         puzzle6.SetActive(false);
+        CubeScript.act.puzzle6.SetActive(false);
         backgroundColor.SetActive(false);
         finishBackground.SetActive(true);
     }

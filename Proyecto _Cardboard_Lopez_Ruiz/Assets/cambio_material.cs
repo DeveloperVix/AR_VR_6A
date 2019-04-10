5a47c244ff71ffcb5c8be12cb4499347b60db064
vicsay1 <36805512+vicsay1@users.noreply.github.com>
2019-04-10 18:14:20 -0500

    Ejercicio_CambiaColor
    Proyecto cardboard: integracion de el package de GoogleVR y adicion de un cubo que cambia de colores dependiendo de si lo estan viendo directamente.

---
+++ Proyecto _Cardboard_Lopez_Ruiz/Assets/cambio_material.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambio_material : MonoBehaviour
+{
+    
+    public Material cubo_material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void sobre()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+        Debug.Log("lol sobres");
+    }
+    public void fuera()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+        Debug.Log("lol fuera");
+    }
+}

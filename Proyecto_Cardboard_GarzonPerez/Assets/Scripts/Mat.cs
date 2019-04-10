f8a446263081094e77fe37c9c698cf82cd8c6cc0
AbrahamGarzon <thesnowboox@gmail.com>
2019-04-10 18:22:16 -0500

    Ejercicio _CambioColor
    ProyectoCardboard
    Al poner el puntero en el cubo cambia de color.

---
+++ Proyecto_Cardboard_GarzonPerez/Assets/Scripts/Mat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mat : MonoBehaviour
+{
+    public Material Normal, Cambio;
+    Renderer Ren;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ren = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void cambioColor(bool Col)
+    {
+        Ren.material = Col ? Cambio : Normal;
+    }
+}

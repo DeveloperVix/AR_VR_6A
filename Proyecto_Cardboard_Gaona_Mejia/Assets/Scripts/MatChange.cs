d6216ffc0d904ddb1dd65cfc8a96a358d9d9a429
fab526 <36773024+fab526@users.noreply.github.com>
2019-04-10 18:33:42 -0500

    Ejercicio_CambiaColor
    Summary. Configuramos las player settings, agregamos las carpetas necesarias para utilizar el cardboard y hicimos que cambiar de color la caja al seleccionarla

---
+++ Proyecto_Cardboard_Gaona_Mejia/Assets/Scripts/MatChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatChange : MonoBehaviour
+{
+    MeshRenderer meshrender;
+    public Material PointMaterial;
+    public Renderer obj;
+    public Material UnPointedMaterial;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshrender = GetComponent<MeshRenderer>(); 
+    }
+
+    public void point()
+    {
+        obj.material = PointMaterial;
+    }
+
+    public void pointed()
+    {
+        obj.material = UnPointedMaterial;
+    }
+}

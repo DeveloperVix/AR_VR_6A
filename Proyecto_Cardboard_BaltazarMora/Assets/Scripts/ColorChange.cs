54af08aa79ac6524b26a960e9da4dfb1010bd3e7
Zack098 <37155500+Zack098@users.noreply.github.com>
2019-04-10 18:14:57 -0500

    Ejercicio_CambiaColor
    New Features:
    °Integración de proyecto.
    °El cubo cambia de color cuando el jugador se enfoca en el.

---
+++ Proyecto_Cardboard_BaltazarMora/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public Material mat;
+    MeshRenderer mesh;
+
+
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void MatChange(Material mat)
+    {
+        mesh.material = mat;
+    }
+}

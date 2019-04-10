cad61b7a0d0acaee0246fd06c5871ff482d1f20f
LeonardoCV1998 <Leocastrovalladolid@hotmail.com>
2019-04-10 18:18:25 -0500

    Ejercicio_CambiaColor
    Proyecto de cardboard: Se realizo la vista del usuario en el cual aparecen un cubo que cambia de color al apuntar con la mirilla

---
+++ Proyecto_Cardboard_CastroValladolid/Assets/Scripts/Cube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public Material normal;
+    public Material selected;
+    MeshRenderer r;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Detectado()
+    {
+        r.material = selected;
+    }
+
+    public void NoDetectado()
+    {
+        r.material = normal;
+    }
+}

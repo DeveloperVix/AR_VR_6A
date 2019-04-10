f7b4f1bcc71db2c87f102fba68502ba72e5f0fad
JOSE CARLOS MARZANA <car_ma20@yahoo.com.mx>
2019-04-10 18:29:29 -0500

    Ejercicio_CambiaColor
    se implemento que al ver un cubo mientras lo estes viendo cambie de color y si dejas de estar centrado en el regresa a su color normal

---
+++ Proyecto_Cardboard_Magaña_Marzana/Assets/Scripts/INSERTARMATERIAL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class INSERTARMATERIAL : MonoBehaviour
+{
+    public Material materialdefecto, selected;
+    private MeshRenderer taco;
+    // Start is called before the first frame update
+    void Start()
+    {
+        taco = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Changecolor()
+    {
+        taco.material = materialdefecto;
+    }
+    public void ReturnColor()
+    {
+        taco.material = selected;
+    }
+}

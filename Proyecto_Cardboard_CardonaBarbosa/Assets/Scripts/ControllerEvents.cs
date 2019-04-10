23e09e257d4cc858e36f51e5de060e902ebb8fa7
BenCarBa <benjas12pumas@hotmail.com>
2019-04-10 18:18:51 -0500

    Ejercicio_CambioColor
    new features: Integracion proyecto VrCardboard.
    Se subio el package de Google VR.
    Se integro un script para hacer el cambio de material de un objeto al momento de una entrada tipo trigger.

---
+++ Proyecto_Cardboard_CardonaBarbosa/Assets/Scripts/ControllerEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerEvents : MonoBehaviour
+{
+    public Material materialrojo;
+    public Renderer cubo;
+    public Material materialNormal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubo = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onChangeMaterial()
+    {
+        cubo.material = materialrojo;
+        
+    }
+    public void onExiteMaterial()
+    {
+        cubo.material = materialNormal;
+    }
+}

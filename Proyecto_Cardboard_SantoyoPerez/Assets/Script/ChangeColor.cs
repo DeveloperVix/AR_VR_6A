29d2c14ce620bb4626abf05955a27b5080d99b10
Benny Santoyo Pérez <maggot15aseeck@gmail.com>
2019-04-10 18:15:44 -0500

    Ejercicio_CambioColor
    LO MISMO QUE EL ANTERIOR x2 xd

---
+++ Proyecto_Cardboard_SantoyoPerez/Assets/Script/ChangeColor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Material Normal;
+    public Material PointEnter;
+    //public Material MaterialAzul, MaterialAmarillo, MaterialRosa, MaterialVerde, MaterialRojo;//Crea dos variables del tipo Material 
+    public MeshRenderer meshRenderer;
+    //public int rand = Random.Range(0,4);
+
+    public virtual void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();//obtener material 
+    }
+
+    public virtual void OnPointerEnter()
+    {
+        Debug.Log("CAMBIANDO DE COLOR");
+        meshRenderer.material = PointEnter;
+    }
+
+    public virtual void OnPointerExit()
+    {
+        Debug.Log("CAMBIANDO DE COLOR");
+        meshRenderer.material = Normal;
+    }
+}

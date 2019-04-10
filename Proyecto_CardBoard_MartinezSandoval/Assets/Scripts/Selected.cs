b48b0d2dff5f81e4e9c5de8e7ff90488bae1a574
Ulikander <44560697+LuckierList@users.noreply.github.com>
2019-04-10 18:11:06 -0500

    Ejercicio_CambiaColor
    ProyectoDeCardboard:
    
    Al poner el puntero en el cubo, este cambia de color, y cuando se le quita de encima, regresa al original.

---
+++ Proyecto_CardBoard_MartinezSandoval/Assets/Scripts/Selected.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selected : MonoBehaviour
+{
+
+    public Material matNormal, matSelect;
+    Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HoverChange(bool val)
+    {
+        renderer.material = val ? matSelect : matNormal;
+    }
+}

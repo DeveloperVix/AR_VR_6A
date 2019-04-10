62b88b470702e21dcae5ecc9a5882dd9f64ac1e8
Benjamin E <36773982+dracober@users.noreply.github.com>
2019-04-10 18:37:32 -0500

    Ejercicio_CambiaColor
    project cardboard
    - Se agrego el paquete de googlecardboard para la realidad virtual tmbn se configuro el build y las player settings. Se añadio un objeto vacio para controlar el puntero de RV y  se agrego un objeto 3D con las funcion de cambiar materiales por medio de un script

---
+++ Proyecto_Cardboard_RamirezRamos/Assets/Scripts/Pointing.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pointing : MonoBehaviour
+{
+    //MeshRenderer MeshRndr;
+    //public Material[] materials;
+    public Material PointedMaterial;
+    public Material UnpointedMaterial;
+    public Renderer obj;
+
+   
+    void Start()
+    {
+        //MeshRndr = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Point()
+    {
+        obj.material = PointedMaterial;
+    }
+
+    public void Unpointed()
+    {
+        obj.material = UnpointedMaterial;
+    }
+}

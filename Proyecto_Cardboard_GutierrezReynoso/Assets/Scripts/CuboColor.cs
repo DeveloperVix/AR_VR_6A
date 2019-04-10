2f84fe9528d89b45aa6740736e0357a0d498682d
Victor Gutierrez <36493042+vicgots@users.noreply.github.com>
2019-04-10 18:25:37 -0500

    Ejercicio_CambiaColor
    -New features: Se ha agregado el proyecto de Realidad Virtual por medio de Google Cardboard.
    Se ha añadido un cubo que cuando el jugador lo esta observando, este cambia de color, una vez que le quita la vista de encima vuelve a su color inicial.

---
+++ Proyecto_Cardboard_GutierrezReynoso/Assets/Scripts/CuboColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboColor : MonoBehaviour
+{
+    
+    MeshRenderer meshRen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRen = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeMaterial(Material mat)
+    {
+        meshRen.material = mat;
+    }
+}

a5e1319b05e1205c1f42528f492a42c706452e42
Eddbros <36971892+Eddbros@users.noreply.github.com>
2019-04-11 01:22:37 -0500

    Ejercicio_CambioColor_Rotar
    -Se agrego un paquete de Google VR
    -Se crearon 2 cubos
    -Se creó un Plano para el piso
    -Se crearon material para los cubos, el piso y para cuando el cubo cambie de color
    -A la camara se le agregaron los componentes del Google VR
    -A la camara se le agrego la reticula que tiene la función de un raycast
    -Se agrego un script con metodos para cambiar el color a un cubo, y en el mismo se agregaron otros metodos para hacer rotar el segundo cubo

---
+++ Proyecto_Cardboard_Valdez_Escalante/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+
+    public Material defaultMaterial, selected;
+    private MeshRenderer meshrender;
+    bool isRotating = false;
+
+
+    float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshrender = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRotating == true)
+        {
+            gameObject.transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+        }
+    }
+
+    public void ChangeColor()
+    {
+        meshrender.material = defaultMaterial;
+    }
+
+    public void ReturnColor()
+    {
+        meshrender.material = selected;
+    }
+
+
+    public void RotateCube()
+    {
+        isRotating = true;
+    }
+
+    public void NoRotate()
+    {
+        isRotating = false;
+    }
+}

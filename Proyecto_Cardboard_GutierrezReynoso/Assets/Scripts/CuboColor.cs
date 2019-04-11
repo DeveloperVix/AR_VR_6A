0a6a518d89e61f004b7960b411d6c4d9c5d8923d
Victor Gutierrez <36493042+vicgots@users.noreply.github.com>
2019-04-10 19:10:54 -0500

    Ejercicio_Rotar
    -New features: Se ha añadido otro cubo que rota en X y Z al momento de ser visto por el jugador, este deja de rotar al momento de dejarlo de visualizar

--- Proyecto_Cardboard_GutierrezReynoso/Assets/Scripts/CuboColor.cs
+++ Proyecto_Cardboard_GutierrezReynoso/Assets/Scripts/CuboColor.cs
@@ -6,6 +6,7 @@ public class CuboColor : MonoBehaviour
 {
     
     MeshRenderer meshRen;
+    public bool flag;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +17,19 @@ public class CuboColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (flag)
+        {
+            transform.Rotate(new Vector3(1, 0, 5));
+        }
     }
 
     public void changeMaterial(Material mat)
     {
         meshRen.material = mat;
+    }
+
+    public void rotateCube(bool bandera)
+    {
+        flag = bandera;
     }
 }

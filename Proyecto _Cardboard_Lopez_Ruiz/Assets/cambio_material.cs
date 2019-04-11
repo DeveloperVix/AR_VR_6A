8650663a547efc0aaf4aaba5e84cd6d6c8e23284
vicsay1 <36805512+vicsay1@users.noreply.github.com>
2019-04-10 19:09:46 -0500

    Ejercicio_rotar
    se añadio otro cubo que rota cuando lo estas viendo

--- Proyecto _Cardboard_Lopez_Ruiz/Assets/cambio_material.cs
+++ Proyecto _Cardboard_Lopez_Ruiz/Assets/cambio_material.cs
@@ -6,16 +6,27 @@ public class cambio_material : MonoBehaviour
 {
     
     public Material cubo_material;
+    public GameObject cubo2;
+    bool rota = false;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rota = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (rota == true)
+        {
+            cubo2.transform.Rotate(1, 1, 1 * 2);
+        }
+        if(rota == false)
+        {
+            cubo2.transform.Rotate(1*0, 1*0, 1 * 0);
+        }
     }
     public void sobre()
     {
@@ -26,5 +37,13 @@ public class cambio_material : MonoBehaviour
     {
         GetComponent<Renderer>().material.color = Color.blue;
         Debug.Log("lol fuera");
+    }
+    public void rotacion_cubo()
+    {
+        rota = true;
+    }
+    public void detener_cubo()
+    {
+        rota = false;
     }
 }

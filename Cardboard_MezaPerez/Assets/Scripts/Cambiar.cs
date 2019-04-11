a7000d372443e7389e491ee9a54c93963d99c858
mezaaaron <36971712+mezaaaron@users.noreply.github.com>
2019-04-10 19:12:16 -0500

    Ejercicio_RotarYCambiarColor
    Al detectar que el jugar está mirando algún cubo este rotará o cambiará su color, y al dejar de verlo dejará de hacerlo.

---
+++ Cardboard_MezaPerez/Assets/Scripts/Cambiar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cambiar : MonoBehaviour
+{
+
+    public Material C1, C2;
+    public MeshRenderer OBJ;
+    void Start()
+    {
+        OBJ = GetComponent<MeshRenderer>();
+    }
+
+    public void pointEnter()
+    {
+        OBJ.material = C2;
+    }
+    public void pointExit()
+    {
+        OBJ.material = C1;
+    }
+}
---
+++ Cardboard_MezaPerez/Assets/Scripts/RotCubo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotCubo : MonoBehaviour
+{
+    public Transform OBJ;
+    bool rotarsi;
+    public float vel;
+    void Start()
+    {
+        OBJ = transform;
+    }
+
+
+
+    private void Update()
+    {
+
+        
+        if (rotarsi)
+        {
+            OBJ.transform.Rotate(0,vel,0);
+        }
+        else
+        {
+            rotarsi = false;
+        }
+    }
+
+
+
+    public void pointEnter()
+    {
+        rotarsi = true;
+    }
+    public void pointExit()
+    {
+        rotarsi = false;
+    }
+}

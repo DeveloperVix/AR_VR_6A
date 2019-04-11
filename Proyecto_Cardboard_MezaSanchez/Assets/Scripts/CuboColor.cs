9a242b24fa164f541f3dfb6ad09fdc45ca3dd8d9
Wink <emanuel.wk13@gmail.com>
2019-04-10 21:24:01 -0500

    Ejercicio_CambiarColor_Rotar
    New features:
    - Proyecto en carborad añadido cubo que se rota o cambia de color segun su configuracion

---
+++ Proyecto_Cardboard_MezaSanchez/Assets/Scripts/CuboColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuboColor : MonoBehaviour
+{
+    
+    MeshRenderer meshRen;
+    public bool flag;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRen = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flag)
+        {
+            transform.Rotate(new Vector3(0, 0, 10));
+        }
+    }
+
+    public void changeMaterial(Material mat)
+    {
+        meshRen.material = mat;
+    }
+
+    public void rotateCube(bool bandera)
+    {
+        flag = bandera;
+    }
+}

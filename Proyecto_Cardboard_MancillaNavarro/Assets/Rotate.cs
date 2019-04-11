9e83eca11b844c0f8f8c5d8329e7e2196d280518
CORSMAN <kevin_corsman@hotmail.com>
2019-04-10 22:39:42 -0500

    Asunto: Ejercicio_RotarYCambiarColor. Descripcion: se crearon cubos, que al interactuar con ellos pasan ciertos eventos, como son rotar y cambiar de color. New features: Se creo un nuevo proyecto en el cual se importo un package apra desarrollar en cardboard

---
+++ Proyecto_Cardboard_MancillaNavarro/Assets/Rotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public Transform cuboRot;
+    bool rot = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rot)
+        {
+            cuboRot.Rotate(0, 0, 100 * Time.deltaTime);
+        }
+    }
+    public void Rotation()
+    {
+        rot = true;
+    }
+    public void ExitRotation()
+    {
+        rot = false;
+    }
+}

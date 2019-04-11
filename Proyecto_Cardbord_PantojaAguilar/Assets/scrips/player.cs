ef47927354cef8a41e04dc11d353fb3b1ff65224
Hector <hector.bazzinga.dp@gmail.com>
2019-04-10 23:39:34 -0500

    Ejecicio_RotarYCambiarColor
    Se creo un poryecto para VR  en el se ponen unos cubos para probar la deteccion de objetos uno al verlo cambia materia y el otro rota.
    BUG: no se porque no subio el proyecto solo borro el anterio metoco cerrar y reiniciar computadora para que lo reconociera

---
+++ Proyecto_Cardbord_PantojaAguilar/Assets/scrips/player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    MeshRenderer mesRender;
+    public Material[] mMatelia;
+    public bool rota = false;
+    public float vely = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesRender = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rota==true) {
+            gameObject.transform.Rotate(new Vector3(0f, vely, 0f) * Time.deltaTime);
+        }
+        
+    }
+    public void entra() {
+        mesRender.material = mMatelia[1];
+    }
+    public void sal() {
+        mesRender.material = mMatelia[0];
+    }
+    public void entraR()
+    {
+        rota = true;
+    }
+    public void salR()
+    {
+        rota = false;
+    }
+}

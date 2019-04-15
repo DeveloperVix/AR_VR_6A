ce20ec93edb959564696c7a56df3a46e4fc57578
Benny Santoyo Pérez <maggot15aseeck@gmail.com>
2019-04-14 23:09:54 -0500

    Ejercicio_CuboGirando
    Se implemento un segundo cubo que gire cuando lo observes y se detenga cuando dejes de observarlo xdd olvidé subir commit saliendo de la clase jejetl

--- Proyecto_Cardboard_SantoyoPerez/Assets/Script/OnPointer.cs
+++ Proyecto_Cardboard_SantoyoPerez/Assets/Script/OnPointer.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChangeColor : MonoBehaviour
+public class OnPointer : MonoBehaviour
 {
     public Material Normal;
     public Material PointEnter;
-    //public Material MaterialAzul, MaterialAmarillo, MaterialRosa, MaterialVerde, MaterialRojo;//Crea dos variables del tipo Material 
     public MeshRenderer meshRenderer;
-    //public int rand = Random.Range(0,4);
 
     public virtual void Start()
     {
-        meshRenderer = GetComponent<MeshRenderer>();//obtener material 
+        meshRenderer = GetComponent<MeshRenderer>();//obtener material
     }
 
     public virtual void OnPointerEnter()
---
+++ Proyecto_Cardboard_SantoyoPerez/Assets/Script/Rotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Transform cubo;
+    public float rotationSpeed;
+    public bool rotando = false;
+
+    public void Start()
+    {
+        cubo = GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        if (rotando == true)
+        {
+            cubo.transform.Rotate(new Vector3(rotationSpeed, rotationSpeed, rotationSpeed) * Time.deltaTime);
+        }
+
+        if (rotando == false)
+        {
+            cubo.transform.Rotate(new Vector3(0f, 0f, 0f) * Time.deltaTime);
+        }
+    }
+
+    public void OnPointerEnter2()
+    {
+        Debug.Log("Rotando");
+        rotando = true;
+    }
+
+    public void OnPointerExit2()
+    {
+        Debug.Log("Pausando Rotacion");
+        rotando = false;
+    }
+}

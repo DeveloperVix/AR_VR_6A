2a7ee0768659867d6a30dc9e7631890371ba8e8e
AbrahamGarzon <thesnowboox@gmail.com>
2019-04-25 23:30:37 -0500

    Actualizacion_Avance
    *Actualizacion de rotacion

--- Proyecto_Cardboard_GarzonPerez/Assets/Scripts/Mat.cs
+++ Proyecto_Cardboard_GarzonPerez/Assets/Scripts/Mat.cs
@@ -6,18 +6,14 @@ public class Mat : MonoBehaviour
 {
     public Material Normal, Cambio;
     Renderer Ren;
-    // Start is called before the first frame update
     void Start()
     {
         Ren = GetComponent<Renderer>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
     }
-    public void cambioColor(bool Col)
+    public virtual void cambioColor(bool Col)
     {
         Ren.material = Col ? Cambio : Normal;
     }
---
+++ Proyecto_Cardboard_GarzonPerez/Assets/Scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : Mat
+{
+    bool detected = false;
+    void Update()
+    {
+        if (!detected)
+        {
+            transform.Rotate(0, 10 * Time.deltaTime, 0);
+        }
+        else if (detected) ;
+        transform.Rotate(0, 0, 0);
+    }
+    public override void cambioColor(bool col)
+    {
+        base.cambioColor(col);
+        detected = col;
+    }
+}

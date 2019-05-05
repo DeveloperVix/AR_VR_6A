c6ab536968cf622740f4e276a761347ae3817eac
LeonardoCV1998 <Leocastrovalladolid@hotmail.com>
2019-05-05 04:17:26 -0500

    Tarea_NivelVR
    Se desarrollo un pequeño nivel donde se utilizaron luces oscilantes de diferentes colores y muros movibles, en el cual tambien se presentan diferentes obstaculos como cierras que rotan sobre su propio eje y "dragones" que por el momento no ejecutan ninguna accion

---
+++ BaseVR_CastroValladolid/Assets/Scripts/LuzColor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuzColor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float duration = 1.0f;
+    public Color color0 ;
+    public Color color1;
+
+    Light lt;
+
+    void Start()
+    {
+        lt = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        // set light color
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lt.color = Color.Lerp(color0, color1, t);
+    }
+}

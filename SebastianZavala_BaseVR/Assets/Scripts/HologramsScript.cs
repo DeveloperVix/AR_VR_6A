7c8bf4a76affc8d8f8aaed39f4e5c3c14e9e2985
BastianZavala <BastianZavala@users.noreply.github.com>
2019-05-06 12:43:23 -0500

    Tarea_VR
    Se agrego esfera para cabeza
    Se agrego metodo de rotacion lateral del jugador
    Se agrego holograma
    bugs:
    El holograma aparece enfrente del jugador y no en el panel
    La camara suele voltearse al revez cuando el jugador se mueve muy rapido

---
+++ SebastianZavala_BaseVR/Assets/Scripts/HologramsScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HologramsScript : MonoBehaviour
+{
+    public Material MaterialA, MaterialB;
+    private MeshRenderer MeshRender;
+    public GameObject screen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRender = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeMaterial()
+    {
+        MeshRender.material = MaterialA;
+        screen.SetActive(true);
+    }
+
+    public void DefaultMaterial()
+    {
+        MeshRender.material = MaterialB;
+        screen.SetActive(false);
+    }
+}

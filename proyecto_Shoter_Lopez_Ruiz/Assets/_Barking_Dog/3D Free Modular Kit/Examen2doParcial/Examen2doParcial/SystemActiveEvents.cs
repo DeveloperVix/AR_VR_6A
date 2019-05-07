76e14589d7a2de0a6bf0f523bfb318f65e88c692
vicsay1 <36805512+vicsay1@users.noreply.github.com>
2019-05-06 19:41:56 -0500

    Examen2doParcial_Lopez_Ruiz
    Aparecen el texto y la imagen Se cambian de color el texto y la imagen tocando los diferentes cilindros y ademas desaparecen despues de un tiempo. ademas se creo una instancia para poder acceder desde otros scrips a los metodos del scrip.

---
+++ proyecto_Shoter_Lopez_Ruiz/Assets/_Barking_Dog/3D Free Modular Kit/Examen2doParcial/Examen2doParcial/SystemActiveEvents.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SystemActiveEvents : MonoBehaviour
+{
+    /*  Este sistema nos servira para mostrar en momentos en especifico del cierto tipos de elementos como:
+        -Texto
+        -Imagenes
+        -Señales o pistas que tendrán un color en especifico
+     */
+    public GameObject texto, imagen_gameobject;
+    public static SystemActiveEvents instance;
+    //Definir un singleton para que podamos acceder desde otros scripts a los metodos publicos de este
+
+    //Definir los colores que se le podrán asignar a los sprites
+
+    //Definir los sprtes que serán las diferentes señales o pistas
+    public string textostring = "esto es un texto loko", textostring2 = "ayuda";
+    //La imagen en el UI que va a mostrar la retroalimentación de las señasl o pistas
+
+    //El texto en la UI donde pondremos el texto
+
+
+    // Use this for initialization
+    void Start () 
+	{
+	
+	}
+
+    public void mostrar_texto(Text textoprov)
+    {
+        texto.gameObject.SetActive(true);
+        texto.GetComponent<Text>().text = textostring;
+    }
+    public void imagen()
+    {
+        imagen_gameobject.SetActive(true);
+    }
+    public void dejar__mostrarlo()
+    {
+        StartCoroutine("hola");
+    }
+    public void cambiar_colores()
+    {
+        texto.GetComponent<Text>().text = textostring2;
+        imagen_gameobject.GetComponent<Image>().color = Color.blue;
+        texto.GetComponent<Text>().color = Color.blue;
+    }
+    public void cambiar_colores2()
+    {
+        imagen_gameobject.GetComponent<Image>().color = Color.red;
+        texto.GetComponent<Text>().color = Color.red;
+    }
+
+    IEnumerator hola()
+    {
+        yield return new WaitForSeconds(2f);
+        texto.gameObject.SetActive(false);
+        imagen_gameobject.SetActive(false);
+    }
+
+    //Metodo publico para mostrar texto, recibe de parametro el texto a mostrar
+    //Mostrar el texto
+    //Asignar texto
+
+    //Metodo para mostrar la imagen
+    //Mostrar la imagen
+    //Asignar la imagen
+    //Asignarle el color
+
+    //Metodo publico para dejar de mostrar los elementos, sea texto o imagen
+    //Pasado cierto tiempo
+    //Desactivar los elementos
+}

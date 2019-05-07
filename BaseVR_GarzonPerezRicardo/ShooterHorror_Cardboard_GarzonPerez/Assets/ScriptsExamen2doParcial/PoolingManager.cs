ce403c19b00fce332f0810210cf6383a223d4010
AbrahamGarzon <thesnowboox@gmail.com>
2019-05-06 19:39:00 -0500

    Examen2doParcial_GarzonPerez
    *Creacion de patron de alberca
    *Creacion de eventos en escena
    PD: Codigo incompleto por tiempo.

---
+++ BaseVR_GarzonPerezRicardo/ShooterHorror_Cardboard_GarzonPerez/Assets/ScriptsExamen2doParcial/PoolingManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingManager : MonoBehaviour
+{
+    //Declarar un singleton para poder acceder a los metodos publicos en otros scripts
+    public static PoolingManager Examen;
+
+    //Necesitamos el prefab para meterlo en la alberca
+    public GameObject PreAlberca;
+
+    //Definir el tamaño de la alberca
+    public int TamAlberca = 5;
+
+    //Definir la cola (palabra reservada Queue) que sera nuestra alberca
+    Queue <GameObject> AlbercaFin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Inicializar la alberca cuando se hace la carga de juego InitPool();
+        InitPool();
+    }
+    //Definir metodo para inicializar la alberca
+    //Crear la cola
+    //Con un ciclo vamos a crear los objetos en escena en base al tamaña de la alberca
+    //Definir una variable del tipo GameObject e igualarle la instancia(Instantiate(prefab) NO NECESITA POSICION NI ROTACION) del objeto a crear
+    //Meter dentro de la cola el objeto (nameCola.Enqueue(variableGameObject definida arriba)) palabra reservada "Enqueue" para encolar el nuevo objeto
+    //Desactivar el objeto creado (variableGameObject definida arriba)
+
+    public void InitPool()
+    {
+        for (int i = 0; i < TamAlberca; i++)
+        {
+            GameObject Prefab = Instantiate(PreAlberca);
+            AlbercaFin.Enqueue(PreAlberca);
+            PreAlberca.SetActive(false);
+        }
+    }
+    //Definir metodo el cual se va a mandar llamar en la parte en donde dispara el jugador, el cual va a retornar un objeto
+    //y recibe de parametros la posición y rotación
+    //Definir una variable del tipo GameObject e igualarle el ultimo objeto que esta en la cola (nameCola.Dequeue), usando la palabra reservada Dequeue
+    //Teniendo en nuestra variable del tipo GameObject de arriba, activar el objeto
+    //Asignarle la nueva posiciocion y rotacion que esta recibiendo como parametros el metodo
+    //Retornar el objeto, dado que nuestro metodo es del tipo GameObject
+
+    public GameObject Shoot(Vector3 newpos, Quaternion newrot)
+    {
+        GameObject Activar;
+        Activar = AlbercaFin.Dequeue();
+        Activar.SetActive(true);
+        Activar.transform.position = newpos;
+        Activar.transform.rotation = newrot;
+        return Activar;
+    }
+        //Este metodo mandarlo llamar cuando ya no necesitamos la bala, recibe de parametro el objeto que vamos a regresar a la alberca
+        //Desactivar el objeto que se esta recibiendo como parametro
+        //Meter el objeto a la cola("Alberca") usando Enqueue("Encolar")
+}
--- BaseVR_GarzonPerezRicardo/ShooterHorror_Cardboard_GarzonPerez/Assets/ScriptsExamen2doParcial/SystemActiveEvents.cs
+++ BaseVR_GarzonPerezRicardo/ShooterHorror_Cardboard_GarzonPerez/Assets/ScriptsExamen2doParcial/SystemActiveEvents.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SystemActiveEvents : MonoBehaviour
 {
@@ -11,33 +12,41 @@ public class SystemActiveEvents : MonoBehaviour
      */
 
     //Definir un singleton para que podamos acceder desde otros scripts a los metodos publicos de este
-
+    public static SystemActiveEvents Sistema;
     //Definir los colores que se le podrán asignar a los sprites
-
+    public Color rojo, verde, naranja;
     //Definir los sprtes que serán las diferentes señales o pistas
-	
+    public Sprite sprite;
     //La imagen en el UI que va a mostrar la retroalimentación de las señasl o pistas
-
+    public Image ImagenPos, ImageRes;
     //El texto en la UI donde pondremos el texto
-
+    public Text TextoColocado;
 	//0 = rigth, 1 = left
-
-    // Use this for initialization
-    void Start () 
-	{
-	
-	}
 
     //Metodo publico para mostrar texto, recibe de parametro el texto a mostrar
         //Mostrar el texto
         //Asignar texto
-
+    public void Texto()
+    {
+        TextoColocado.gameObject.SetActive(true);
+        TextoColocado.text = "Hola que hace";
+    }
     //Metodo para mostrar la imagen
         //Mostrar la imagen
         //Asignar la imagen
         //Asignarle el color
-    
+    public void Imagen()
+    {
+        ImagenPos.gameObject.SetActive(true);
+        ImagenPos.color = rojo;
+        ImagenPos.sprite = sprite;
+    }
     //Metodo publico para dejar de mostrar los elementos, sea texto o imagen
         //Pasado cierto tiempo
             //Desactivar los elementos
+    public void Desactivar()
+    {
+        TextoColocado.gameObject.SetActive(false);
+        ImagenPos.gameObject.SetActive(false);
+    }
 }

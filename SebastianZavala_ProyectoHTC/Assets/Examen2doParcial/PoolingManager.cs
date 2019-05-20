f305fb7e0fa90d3a98884bb00a1bd2d22c2752d6
BastianZavala <BastianZavala@users.noreply.github.com>
2019-05-20 17:21:02 -0500

    cleaning

---
+++ SebastianZavala_ProyectoHTC/Assets/Examen2doParcial/PoolingManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolingManager : MonoBehaviour
+{
+    //Declarar un singleton para poder acceder a los metodos publicos en otros scripts    
+
+    //Necesitamos el prefab para meterlo en la alberca
+    public GameObject objeto1;
+    //Definir el tamaño de la alberca
+    int cucumber = 5;
+    //Definir la cola (palabra reservada Queue) que sera nuestra alberca
+    private Queue MegaPool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Inicializar la alberca cuando se hace la carga de juego InitPool();
+    }
+
+    //Definir metodo para inicializar la alberca
+    public void InitPool()
+    {
+        
+    }
+    //Crear la cola
+    //Con un ciclo vamos a crear los objetos en escena en base al tamaña de la alberca
+    //Definir una variable del tipo GameObject e igualarle la instancia(Instantiate(prefab) NO NECESITA POSICION NI ROTACION) del objeto a crear
+    //Meter dentro de la cola el objeto (nameCola.Enqueue(variableGameObject definida arriba)) palabra reservada "Enqueue" para encolar el nuevo objeto
+    //Desactivar el objeto creado (variableGameObject definida arriba)
+
+
+
+    //Definir metodo el cual se va a mandar llamar en la parte en donde dispara el jugador, el cual va a retornar un objeto
+    //y recibe de parametros la posición y rotación
+    public void Shooting()
+    {
+
+    }
+
+    //Definir una variable del tipo GameObject e igualarle el ultimo objeto que esta en la cola (nameCola.Dequeue), usando la palabra reservada Dequeue
+    //Teniendo en nuestra variable del tipo GameObject de arriba, activar el objeto
+    //Asignarle la nueva posiciocion y rotacion que esta recibiendo como parametros el metodo 
+    //Retornar el objeto, dado que nuestro metodo es del tipo GameObject
+
+
+    //Este metodo mandarlo llamar cuando ya no necesitamos la bala, recibe de parametro el objeto que vamos a regresar a la alberca
+    public void Swiming()
+    {
+        
+    }
+
+        //Desactivar el objeto que se esta recibiendo como parametro
+        //Meter el objeto a la cola("Alberca") usando Enqueue("Encolar")
+}
---
+++ SebastianZavala_ProyectoHTC/Assets/Examen2doParcial/SystemActiveEvents.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SystemActiveEvents : MonoBehaviour
+{
+    /*  Este sistema nos servira para mostrar en momentos en especifico del cierto tipos de elementos como:
+        -Texto
+        -Imagenes
+        -Señales o pistas que tendrán un color en especifico
+     */
+
+    //Definir un singleton para que podamos acceder desde otros scripts a los metodos publicos de este
+
+
+    //Definir los colores que se le podrán asignar a los sprites
+    public MeshRenderer MaterialChamber;
+    
+
+    public Material Material1, Material2, Material3, Material4;
+    //Definir los sprites que serán las diferentes señales o pistas
+    public GameObject signal1, signal2, signal3, signal4;
+    //La imagen en el UI que va a mostrar la retroalimentación de las señasl o pistas
+    public Image img1, img2, img3;
+    //El texto en la UI donde pondremos el texto
+    public Text text1, text2, text3;
+
+	//0 = rigth, 1 = left
+
+    // Use this for initialization
+    void Start () 
+	{
+        MaterialChamber.GetComponent<MeshRenderer>();
+        img1.GetComponent<Text>();
+
+        TextChamber();
+        ImageChameber();
+        HidenChambers();
+	
+	}
+
+    //Metodo publico para mostrar texto, recibe de parametro el texto a mostrar
+    //Mostrar el texto
+    //Asignar texto
+
+    public void TextChamber()
+    {
+        text1.text = "Banana";
+        text2.text = "Kurt Escopetas";
+        text3.text = "Salmon Dance";
+    }
+
+    //Metodo para mostrar la imagen
+    //Mostrar la imagen
+    //Asignar la imagen
+    //Asignarle el color
+
+    public void ImageChameber()
+    {
+
+    }
+
+    //Metodo publico para dejar de mostrar los elementos, sea texto o imagen
+    //Pasado cierto tiempo
+    //Desactivar los elementos
+
+    public void HidenChambers()
+    {
+        
+    }
+   
+}
